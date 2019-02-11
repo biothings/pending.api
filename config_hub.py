17659d39322b06ad923047fd318436cb37b52248
Sebastien Lelong <sebastien.lelong@gmail.com>
2019-02-11 20:02:16 +0000

    missing collection nam config

--- config_hub.py
+++ config_hub.py
@@ -7,6 +7,7 @@ DATA_HUB_DB_DATABASE = "biothings_hubdb"   # db containing the following (intern
 DATA_SRC_MASTER_COLLECTION = 'src_master'  # for metadata of each src collections
 DATA_SRC_DUMP_COLLECTION = 'src_dump'      # for src data download information
 DATA_SRC_BUILD_COLLECTION = 'src_build'    # for src data build information
+DATA_SRC_BUILD_CONFIG_COLLECTION = 'src_build_config'
 DATA_PLUGIN_COLLECTION = 'data_plugin'     # for data plugins information
 API_COLLECTION = 'api'                     # for api information (running under hub control)
 CMD_COLLECTION = 'cmd'                     # for cmd launched from the hub
