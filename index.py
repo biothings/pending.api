01e8354f1b9477bc9cd3d368226e3d6bb15b96bf
Jerry <xzhou@scripps.edu>
2021-10-26 21:06:05 -0700

    entry point 0.10.x upgrade

--- index.py
+++ index.py
@@ -7,4 +7,4 @@ from biothings.web.launcher import main
 from web.handlers import EXTRA_HANDLERS
 
 if __name__ == "__main__":
-    main(EXTRA_HANDLERS)
+    main(EXTRA_HANDLERS, {"static_path": "static"})
