04d968a6eabcd3c09137ab48c385e419236c30ff
kannabhargav <47606677+kannabhargav@users.noreply.github.com>
2022-02-07 13:52:16 -0500

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,6 +5,7 @@ export $(egrep -v '^#' .env)
 sed -i.bak \
     -e "s/DOCKER_VERSION_VALUE/${BUILD_VERSION}/g" \
     -e "s/BTE_ALLOWED_HOSTS_VALUE/${BTE_ALLOWED_HOSTS}/g" \
+    -e "s/ES_HOST_VALUE/${ES_HOST}/g" \
     deployment.yaml
 rm deployment.yaml.bak
 
@@ -15,7 +16,7 @@ sed -i.bak \
     -e "s/ENVIRONMENT_TAG_VALUE/${ENVIRONMENT_TAG}/g" \
     ingress.yaml
 rm ingress.yaml.bak
-export ES_HOST=0.0.0.0
+
 kubectl apply -f namespace.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f services.yaml
