8f093cb61c2e18340071dfc9b0eafbca5df910ad
bettyli037 <jingyi.li@axleinfo.com>
2022-05-04 14:49:21 -0400

    feat: refactor pendingapi

---
+++ deploy/pendingapi/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+projectName="pendingapi"
+namespace="bte"
+
+export $(egrep -v '^#' .env)
+
+sed -i.bak \
+    -e "s/DOCKER_VERSION_VALUE/${BUILD_VERSION}/g" \
+    values.yaml
+rm values.yaml.bak
+
+kubectl apply -f namespace.yaml
+
+helm -n ${namespace} upgrade --install ${projectName} -f values.yaml -f values-btepa.yaml ./
