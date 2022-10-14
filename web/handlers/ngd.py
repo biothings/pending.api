e40b17caffbbe930aaf6e0de7ea721458ce0f0fd
Yao Yao <yaoy.cs@gmail.com>
2022-10-14 15:01:17 -0700

    major refactor: add a new HTTP param 'show-leaves' to control the presence of field 'leaf_umls' in response; 10x the cache sizes; use the abstract TermExpansionService in SemmedNGDHandler; implemented lazy-expansion policy

--- web/handlers/ngd.py
+++ web/handlers/ngd.py
@@ -1,9 +1,8 @@
 from enum import Flag, auto
 from biothings.web.handlers.query import BaseAPIHandler
 
-from web.utils import NGDUndefinedException, UNDEFINED_STR
-from web.service.umls_service import UMLSResourceManager
-from web.service.ngd_service import NGDService, DocStatsService, NGDCache, DocStatsCache, Term, TermPair
+from web.utils import NGDZeroDocFreqException, UNDEFINED_STR
+from web.service.ngd_service import NGDService, DocStatsService, NGDCache, DocStatsCache, Term, TermPair, TermExpansionService
 
 
 class ExpansionMode(Flag):
@@ -38,8 +37,9 @@ class ErrorReason:
         return f"Parameter 'expand' accepts empty string, 'nil, 'left', 'right', and 'both', case-insensitive. Got '{arg_expand}'."
 
     @classmethod
-    def zero_document_freq(cls, term: str):
-        return f"the document frequency of term '{term}' is 0 in SemMedDB API."
+    def zero_document_freq(cls, term: str, expanded: bool):
+        status = " (expanded)" if expanded else ""
+        return f"The document frequency of term '{term}'{status} is 0 in SemMedDB API."
 
     @classmethod
     def terms_not_a_list(cls, terms):
@@ -61,6 +61,10 @@ class SemmedNGDHandler(BaseAPIHandler):
             'expand': {
                 'type': str,
                 'required': False
+            },
+            'show-leaves': {
+                'type': bool,
+                'default': False
             }
         },
         'POST': {
@@ -72,20 +76,25 @@ class SemmedNGDHandler(BaseAPIHandler):
             'expand': {
                 'type': str,
                 'required': False
+            },
+            'show-leaves': {
+                'type': bool,
+                'default': False
             }
         }
     }
 
-    doc_stats_cache = DocStatsCache(unary_capacity=10240, bipartite_capacity=10240)
-    ngd_cache = NGDCache(capacity=10240)
+    # Suppose each term ID is an 8-char string and each doc freq is an integer. An OrderedDict of size 102400 takes ~10MB in RAM
+    doc_stats_cache = DocStatsCache(unary_capacity=102400, bipartite_capacity=102400)
+    ngd_cache = NGDCache(capacity=102400)
 
-    def initialize(self, subject_field_name: str, object_field_name: str, umls_resouce_manager: UMLSResourceManager):
+    def initialize(self, subject_field_name: str, object_field_name: str, term_expansion_service: TermExpansionService):
         super().initialize()
 
         # The following 3 arguments are injected from the URLSpec in config_web/<plugin_name>.py
         self.subject_field_name = subject_field_name
         self.object_field_name = object_field_name
-        self.umls_resouce_manager = umls_resouce_manager
+        self.term_expansion_service = term_expansion_service
 
     def prepare(self):
         super().prepare()
@@ -103,8 +112,8 @@ class SemmedNGDHandler(BaseAPIHandler):
         self.doc_total = self.biothings.metadata.biothing_metadata[self.biothings.config.ES_DOC_TYPE]["stats"]["total"]
 
         self.doc_stats_service = DocStatsService(
-            es_async_client=self.biothings.elasticsearch.async_client,  # injected by handler's application instace (created by APILauncher)
-            es_index_name=self.biothings.config.ES_INDEX,  # injected by handler's application instace (created by APILauncher)
+            es_async_client=self.biothings.elasticsearch.async_client,  # injected by handler's application instance (created by APILauncher)
+            es_index_name=self.biothings.config.ES_INDEX,  # injected by handler's application instance (created by APILauncher)
             subject_field_name=self.subject_field_name,
             object_field_name=self.object_field_name,
             doc_total=self.doc_total
@@ -112,36 +121,51 @@ class SemmedNGDHandler(BaseAPIHandler):
 
         self.ngd_service = NGDService(
             doc_stats_service=self.doc_stats_service,
+            term_expansion_service=self.term_expansion_service,
             doc_stats_cache=self.doc_stats_cache,
             ngd_cache=self.ngd_cache
         )
 
-    def expand_term(self, term: Term):
-        prefix = "UMLS:"
-        prefix_len = len(prefix)
-
-        root_term = term.root
-        if not root_term.startswith(prefix):
-            root_term = "UMLS:" + root_term
-
-        # "narrower_relationships" is a name defined in config_web/semmeddb.py
-        nr_client = self.umls_resouce_manager.get_resource_client("narrower_relationships")
-        leaf_terms = nr_client.query(root_term)
-        if leaf_terms:
-            leaf_terms = [term[prefix_len:] if term.startswith(prefix) else term for term in leaf_terms]
-
-        term.expand(leaf_terms)
-        return term
-
-    def pair_two_terms(self, term_x_root: str, term_y_root: str, expansion_mode: ExpansionMode) -> TermPair:
-        term_x = Term(root=term_x_root)
-        term_y = Term(root=term_y_root)
-        if expansion_mode & ExpansionMode.LEFT:  # expansion_mode is LEFT or BOTH
-            term_x = self.expand_term(term_x)
-        if expansion_mode & ExpansionMode.RIGHT:  # expansion_mode is RIGHT or BOTH
-            term_y = self.expand_term(term_y)
+    @classmethod
+    def pair_two_terms(cls, term_x_root: str, term_y_root: str, expansion_mode: ExpansionMode) -> TermPair:
+        # "expansion_mode & ExpansionMode.LEFT" is true when expansion_mode is LEFT or BOTH
+        # "expansion_mode & ExpansionMode.RIGHT" is true when expansion_mode is RIGHT or BOTH
+        term_x = Term(root=term_x_root, expandable=bool(expansion_mode & ExpansionMode.LEFT))
+        term_y = Term(root=term_y_root, expandable=bool(expansion_mode & ExpansionMode.RIGHT))
         term_pair = TermPair(term_x=term_x, term_y=term_y)
         return term_pair
+
+    async def make_response(self, term_pair: TermPair, expansion_mode: ExpansionMode, show_leaves: bool):
+        response = {}
+
+        try:
+            ngd = await self.ngd_service.calculate_ngd(term_pair)
+        except NGDZeroDocFreqException as e:
+            ngd = UNDEFINED_STR
+
+            # We assume that "e.term" is an instance of Term class
+            # Note that we don't use "e.term.expanded" here because it's not necessarily True at this moment
+            # even if we have indicated to expand it. (due to lazy-expansion)
+            reason = ErrorReason.zero_document_freq(e.term.root, e.term.expandable)
+            response["reason"] = reason
+
+        response["ngd"] = ngd
+        response["umls"] = [term.root for term in term_pair]
+
+        if expansion_mode is not ExpansionMode.NIL:
+            # only write "expand" field in response when expansion mode is not 'nil'
+            response["expand"] = expansion_mode.name.lower()
+
+            # arg "show-leaves" only works when expansion mode is not 'nil'
+            if show_leaves:
+                # The last lazy-expansion step.
+                # Suppose the term pair is (x, y). When calling self.ngd_service.calculate_ngd(), if (x, y) hits the distance cache (i.e. ngd_cache[(x, y)]),
+                # or if it hits all three doc freq caches (i.e. unary_doc_freq[x], unary_doc_freq[y], and bipartite_doc_freq[(x, y)]), the terms are NOT
+                # expanded. So here we must check and expand these two terms if necessary before writing the "leaf_umls" field in the response.
+                self.ngd_service.expand_term_pair(term_pair)
+                response["leaf_umls"] = {term.root: (term.leaves or None) for term in term_pair if term.expanded}
+
+        return response
 
     async def get(self, *args, **kwargs):
         arg_umls = self.args['umls']
@@ -156,34 +180,17 @@ class SemmedNGDHandler(BaseAPIHandler):
             self.write_error(status_code=400, reason=ErrorReason.unknown_expansion_mode(arg_expand))
             return
 
+        arg_show_leaves = self.args['show-leaves']
         term_pair = self.pair_two_terms(term_x_root=arg_umls[0], term_y_root=arg_umls[1], expansion_mode=expansion_mode)
-        try:
-            ngd = await self.ngd_service.calculate_ngd(term_pair)
-            result = {
-                "ngd": ngd
-            }
-        except NGDUndefinedException as ue:
-            # Here we assume that `ue.cause` is an NGDZeroCountException, which is the only possibility so far.
-            # If in the future there are other types of `ue.cause`, should check `type(ne.cause)` first in this clause.
-            causative_term = ue.cause.term
-            reason = ErrorReason.zero_document_freq(causative_term.root)
-            result = {
-                "ngd": UNDEFINED_STR,
-                "reason": reason
-            }
-        finally:
-            result["umls"] = [term.root for term in term_pair]
-            result["expand"] = expansion_mode.name.lower()
-            if expansion_mode is not ExpansionMode.NIL:
-                result["leaf_umls"] = {term.root: term.leaves for term in term_pair if term.has_expanded}
-
-            self.finish(result)
-            return
+        response = await self.make_response(term_pair=term_pair, expansion_mode=expansion_mode, show_leaves=arg_show_leaves)
+        await self.finish(response)
+        return
 
     async def post(self, *args, **kwargs):
         # Step 1: Accept two arguments
         arg_umls = self.args['umls']  # should be a list of 2-termed sublists, e.g. [['a', 'b'], ['c', 'd']]
         arg_expand = self.args.get('expand')
+        arg_show_leaves = self.args['show-leaves']
 
         # Step 2: verify argument `expand`, raise an error simultaneously if found
         try:
@@ -194,8 +201,8 @@ class SemmedNGDHandler(BaseAPIHandler):
 
         # Step 3: verify argument `umls` and calculate NGDs.
         # If any pair of terms failed the verification, do not raise an error simultaneously but wrap the error in the response.
-        async def yield_results(arg_umls: list):
-            for terms in arg_umls:
+        async def yield_responses(terms_list: list):
+            for terms in terms_list:
                 if not isinstance(terms, list):
                     yield {
                         "umls": terms,
@@ -213,28 +220,9 @@ class SemmedNGDHandler(BaseAPIHandler):
                     continue
 
                 term_pair = self.pair_two_terms(term_x_root=terms[0], term_y_root=terms[1], expansion_mode=expansion_mode)
-                try:
-                    ngd = await self.ngd_service.calculate_ngd(term_pair)
-                    result = {
-                        "ngd": ngd
-                    }
-                except NGDUndefinedException as ue:
-                    # Here we assume that `ue.cause` is an NGDZeroCountException, which is the only possibility so far.
-                    # If in the future there are other types of `ue.cause`, should check `type(ne.cause)` first in this clause.
-                    causative_term = ue.cause.term
-                    reason = ErrorReason.zero_document_freq(causative_term.root)
-                    result = {
-                        "ngd": UNDEFINED_STR,
-                        "reason": reason
-                    }
-                finally:
-                    result["umls"] = [term.root for term in term_pair]
-                    result["expand"] = expansion_mode.name.lower()
-                    if expansion_mode is not ExpansionMode.NIL:
-                        result["leaf_umls"] = [{term.root: term.leaves} for term in term_pair if term.has_expanded]
+                response = await self.make_response(term_pair=term_pair, expansion_mode=expansion_mode, show_leaves=arg_show_leaves)
+                yield response
 
-                    yield result
-
-        results = [res async for res in yield_results(arg_umls)]
-        self.finish(results)
+        response_list = [res async for res in yield_responses(arg_umls)]
+        await self.finish(response_list)
         return
