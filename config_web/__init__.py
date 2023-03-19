d885ea897a2a06ed574c2094ea6275b67a1ae990
Yao Yao <yaoy.cs@gmail.com>
2023-03-19 14:48:58 -0700

    sort imports; add semmeddb2 plugin

--- config_web/__init__.py
+++ config_web/__init__.py
@@ -1,70 +1,71 @@
-# from . import fire
-from . import denovodb
+from . import agr
+from . import biggim
+from . import biggim_drugresponse_kp
+from . import bindingdb
 from . import biomuta
-from . import phewas
-from . import kaviar
-from . import geneset
-from . import pfocr
-# from . import cord_genomic_entity
+from . import bioplanet_pathway_disease
+from . import bioplanet_pathway_gene
 from . import ccle
-from . import semmedphenotype
-from . import ebigene2phenotype
-from . import mgigene2phenotype
-from . import semmedchemical
-from . import semmed_anatomy
-# from . import cord_chemical
-from . import semmed
-from . import gwascatalog
-# from . import cord_disease
+from . import cell_ontology
+from . import chebi
 # from . import cord_anatomy
-# from . import cord_protein
-from . import semmedgene
-from . import umlschem
 # from . import cord_bp
-# from . import cord_ma
-from . import diseases
-# from . import cord_cell
-# from . import cord_gene
-from . import semmedbp
 # from . import cord_cc
-from . import mrcoc
-from . import pseudocap_go
-from . import hpo
-from . import uberon
+# from . import cord_chemical
+# from . import cord_cell
+# from . import cord_disease
+# from . import cord_gene
+# from . import cord_genomic_entity
+# from . import cord_ma
+# from . import cord_protein
+from . import ddinter
+from . import denovodb
+from . import dgidb
+from . import diseases
+from . import doid
+from . import ebigene2phenotype
+# from . import fire
+from . import foodb
+from . import fooddata
+from . import geneset
+from . import go
 from . import go_bp
 from . import go_cc
 from . import go_mf
-from . import cell_ontology
-from . import biggim
-# from . import textminingkp
+from . import gtrx
+from . import gwascatalog
+from . import hmdb
+from . import hpo
+from . import idisk
+from . import innatedb
+from . import kaviar
+from . import mgigene2phenotype
+from . import mondo
+from . import mrcoc
+from . import multiomics_clinicaltrials_kp
+from . import multiomics_ehr_risk_kp
+from . import multiomics_wellness
+from . import node_expansion
+from . import pfocr
+from . import phewas
 # from . import phenotype  # replaced by hpo. See https://github.com/biothings/pending.api/issues/84
-from . import biggim_drugresponse_kp
-from . import dgidb
+from . import pseudocap_go
+from . import repodb
+from . import rhea
+from . import semmed
+from . import semmed_anatomy
+from . import semmedbp
+from . import semmedchemical
+from . import semmeddb
+from . import semmeddb2
+from . import semmedgene
+from . import semmedphenotype
+from . import suppkg
 from . import tcga_mut_freq_kp
+# from . import textminingkp
 from . import text_mining_co_occurrence_kp
 from . import text_mining_targeted_association
-from . import upheno_ontology
-from . import repodb
-from . import multiomics_wellness
-from . import multiomics_ehr_risk_kp
-from . import idisk
-from . import agr
-from . import semmeddb
-from . import rhea
-from . import suppkg
-from . import bioplanet_pathway_gene
-from . import bioplanet_pathway_disease
-from . import foodb
-from . import bindingdb
-from . import gtrx
-from . import innatedb
-from . import multiomics_clinicaltrials_kp
-from . import ddinter
-from . import doid
-from . import go
-from . import node_expansion
-from . import mondo
-from . import chebi
 from . import tissues
-from . import hmdb
-from . import fooddata
+from . import uberon
+from . import umlschem
+from . import upheno_ontology
