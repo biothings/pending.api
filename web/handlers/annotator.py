fe4a8066beea5546c4fb84c22d36c72f504b37c2
Chunlei Wu <anewgene@yahoo.com>
2023-08-15 13:31:33 -0700

    feat: allow caching in ResponseTransformer

--- web/handlers/annotator.py
+++ web/handlers/annotator.py
@@ -34,15 +34,27 @@ BIOLINK_PREFIX_to_BioThings = {
 
 
 class ResponseTransformer:
-    def _transform_chembl_drug_indications(self, res):
+    def __init__(self, res_by_id, node_type):
+        self.res_by_id = res_by_id
+        self.node_type = node_type
+
+        self.data_cache = {}    # used to cached required mapping data used for individual transformation
+                                # typically those data coming from other biothings APIs, we will do a batch
+                                # query to get them all, and cache them here for later use, to avoid slow
+                                # one by one queries.
+
+    def _transform_chembl_drug_indications(self, doc):
+        if self.node_type != "chem":
+            return doc
+
         def _append_mesh_prefix(chembl):
             xli = chembl.get("drug_indications", [])
-            for doc in xli:
-                if "mesh_id" in doc:
+            for _doc in xli:
+                if "mesh_id" in _doc:
                     # Add MESH prefix to chembl.drug_indications.mesh_id field
-                    doc["mesh_id"] = append_prefix(doc["mesh_id"], "MESH")
+                    _doc["mesh_id"] = append_prefix(_doc["mesh_id"], "MESH")
 
-        chembl = res.get("chembl", {})
+        chembl = doc.get("chembl", {})
         if chembl:
             if isinstance(chembl, list):
                 # in case returned chembl is a list, rare but still possible
@@ -51,22 +63,61 @@ class ResponseTransformer:
             else:
                 _append_mesh_prefix(chembl)
 
-        return res
+        return doc
 
-    # FIXME: method _transform_add_ncit_description is temporarily disabled. Fix later.
-    def disabled_transform_add_ncit_description(self, res):
-        ncit_api = biothings_client.get_client(url="https://biothings.ncats.io/ncit")
-        def _add_ncit_description(unii):
-            ncit = unii.get("ncit")
-            if ncit:
-                ncit_def = ncit_api.getnode(f"NCIT:{ncit}", fields="def").get("def")
-                if ncit_def:
+    def caching_ncit_descriptions(self):
+        """cache ncit descriptions for all unii.ncit IDs from self.res_by_id"""
+        ncit_id_list= []
+        for res in self.res_by_id.values():
+            if isinstance(res, list):
+                # in case returned res is a list, rare but still possible
+                for r in res:
+                    unii = r.get("unii", {})
+                    if isinstance(unii, list):
+                        for u in unii:
+                            ncit = u.get("ncit")
+                            if ncit:
+                                ncit_id_list.append(ncit)
+                    else:
+                        ncit = unii.get("ncit")
+                        if ncit:
+                            ncit_id_list.append(ncit)
+            else:
+                ncit = res.get("unii", {}).get("ncit")
+                if ncit:
+                    ncit_id_list.append(ncit)
+        if ncit_id_list:
+            ncit_api = biothings_client.get_client(url="https://biothings.ncats.io/ncit")
+            ncit_id_list = [f"NCIT:{ncit}" for ncit in ncit_id_list]
+            ncit_res = ncit_api.getnodes(ncit_id_list, fields="def")
+            ncit_def_d = {}
+            for hit in ncit_res:
+                if hit.get("def"):
+                    ncit_def = hit["def"]
                     # remove the trailing " []" if present
                     if ncit_def.startswith('"') and ncit_def.endswith('" []'):
                         ncit_def = ncit_def[1:-4]
+                    ncit_def_d[hit["_id"]] = ncit_def
+            if ncit_def_d:
+                self.data_cache["ncit"] = ncit_def_d
+
+    def _transform_add_ncit_description(self, doc):
+        if self.node_type != "chem":
+            return doc
+
+        if "ncit" not in self.data_cache:
+            self.caching_ncit_descriptions()
+
+        ncit_def_d = self.data_cache.get("ncit", {})
+        def _add_ncit_description(unii):
+            ncit = unii.get("ncit")
+            ncit = f"NCIT:{ncit}"
+            if ncit:
+                ncit_def = ncit_def_d.get(ncit)
+                if ncit_def:
                     unii["ncit_description"] = ncit_def
 
-        unii = res.get("unii", {})
+        unii = doc.get("unii", {})
         if unii:
             if isinstance(unii, list):
                 # in case returned chembl is a list, rare but still possible
@@ -74,17 +125,26 @@ class ResponseTransformer:
                     _add_ncit_description(u)
             else:
                 _add_ncit_description(unii)
-        return res
+        return doc
 
-    def transform(self, res):
+    def transform_one_doc(self, doc):
         """transform the response from biothings client"""
         for fn_name, fn in inspect.getmembers(self, predicate=inspect.ismethod):
             if fn_name.startswith("_transform_"):
-                if isinstance(res, list):
-                    res = [fn(r) for r in res]
+                if isinstance(doc, list):
+                    doc = [fn(r) for r in doc]
                 else:
-                    res = fn(res)
-        return res
+                    doc = fn(doc)
+        return doc
+
+    def transform(self):
+        for node_id in self.res_by_id:
+            res = self.res_by_id[node_id]
+            if isinstance(res, list):
+                # TODO: handle multiple results here
+                res = [self.transform_one_doc(r) for r in res]
+            else:
+                res = self.transform_one_doc(res)
 
 
 class TRAPIInputError(ValueError):
@@ -230,22 +290,28 @@ class Annotator:
             raise InvalidCurieError(f"Unsupported Curie prefix: {curie}")
         res = self.query_biothings(node_type, [_id], fields=fields)
         if not raw:
-            res = [self.transform(r) for r in res[_id]]
-        return {curie: res}
+            res = self.transform(res, node_type)
+            # res = [self.transform(r) for r in res[_id]]
+        return {curie: res[_id]}
 
-    def transform(self, res):
-        """perform any transformation on the annotation object, but in-place also returned object"""
-        res.pop("query", None)
-        res.pop("_score", None)
-        # now doing field specific transformation
-        transformer = ResponseTransformer()
-        transformer.transform(res)
-        # for path, value in traverse_keys(res):
-        #     if path in ANNOTAION_FIELD_TRANSFORMATION:
-        #         fn = ANNOTAION_FIELD_TRANSFORMATION[path]
-        #         new_value = fn(value)
-        #         set_key_value(res, path, new_value)
-        return res
+    def transform(self, res_by_id, node_type):
+        """perform any transformation on the annotation object, but in-place also returned object
+           res_by_id is the output of query_biothings, node_type is the same passed to query_biothings
+        """
+        logger.info("Transforming output annotations for %s %ss...", len(res_by_id), node_type)
+        transformer = ResponseTransformer(res_by_id, node_type)
+        transformer.transform()
+        logger.info("Done.")
+        ####
+        # if isinstance(res, list):
+        #     # TODO: handle multiple results here
+        #     res = [transformer.transform(r) for r in res]
+        # else:
+        #     res.pop("query", None)
+        #     res.pop("_score", None)
+        #     res = transformer.transform(res)
+        ####
+        return res_by_id
 
     def annotate_trapi(self, trapi_input, append=False, raw=False, fields=None, limit=None):
         """Annotate a TRAPI input message with node annotator annotations"""
@@ -290,15 +356,17 @@ class Annotator:
             # query_id to original id mapping
             node_id_d = dict(zip(query_list, node_list))
             res_by_id = self.query_biothings(node_type, query_list, fields=fields)
+            if not raw:
+                res_by_id = self.transform(res_by_id, node_type)
             for node_id in res_by_id:
                 orig_node_id = node_id_d[node_id]
                 res = res_by_id[node_id]
-                if not raw:
-                    if isinstance(res, list):
-                        # TODO: handle multiple results here
-                        res = [self.transform(r) for r in res]
-                    else:
-                        res = self.transform(res)
+                # if not raw:
+                #     if isinstance(res, list):
+                #         # TODO: handle multiple results here
+                #         res = [self.transform(r) for r in res]
+                #     else:
+                #         res = self.transform(res)
                 res = {
                     "attribute_type_id": "biothings_annotations",
                     "value": res,
