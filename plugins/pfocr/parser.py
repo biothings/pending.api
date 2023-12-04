0b27e78bc1668b6621471b9153358e47676880a0
ctrl-schaff <schaffjohnathan@gmail.com>
2023-12-04 10:58:53 -0800

    Add the three different parser flavor API methods

--- plugins/pfocr/parser.py
+++ plugins/pfocr/parser.py
@@ -1,12 +1,40 @@
-import os
+"""
+biothings.api plugin for parsing three different PFOCR "flavors"
+
+flavors:
+> all
+> synoynms
+> strict
+
+"""
+
+
+from pathlib import Path
 import json
 
 
-def load_data(data_folder):
-    file_path = os.path.join(data_folder, "bte_chemicals_diseases_genes.ndjson")
-    with open(file_path) as f:
+def _load_data(data_file):
+    with open(data_file, "r", encoding="utf-8") as f:
         for line in f.readlines():
             doc = json.loads(line)
-            if not isinstance(doc['associatedWith']['pmc'], str):
-                doc['associatedWith'].pop("pmc")
+            if not isinstance(doc["associatedWith"]["pmc"], str):
+                doc["associatedWith"].pop("pmc")
             yield doc
+
+
+def load_pfocr_all(data_folder):
+    data_file_name = "bte_chemicals_diseases_genes_all.ndjson"
+    data_file = Path(data_folder) / data_file_name
+    yield _load_data(str(data_file))
+
+
+def load_pfocr_synoynms(data_folder):
+    data_file_name = "bte_chemicals_diseases_genes_synoynms.ndjson"
+    data_file = Path(data_folder) / data_file_name
+    yield _load_data(str(data_file))
+
+
+def load_pfocr_strict(data_folder):
+    data_file_name = "bte_chemicals_diseases_genes_strict.ndjson"
+    data_file = Path(data_folder) / data_file_name
+    yield _load_data(str(data_file))
