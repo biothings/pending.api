fec7314e659ced6ea93b76593f83e998aaa000a1
ctrl-schaff <schaffjohnathan@gmail.com>
2023-12-21 11:41:30 -0800

    Update the web configuration to include ES INDICES

--- config_web/pfocr.py
+++ config_web/pfocr.py
@@ -1,27 +1,43 @@
 import copy
-from biothings.web.settings.default import QUERY_KWARGS
+from biothings.web.settings.default import QUERY_KWARGS, ES_INDICES
 
-PFOCR_FLAVOR_ALL = "pending-pfocr-all"
-PFOCR_FLAVOR_SYNONYMS = "pending-pfocr-synonyms"
-PFOCR_FLAVOR_STRICT = "pending-pfocr-strict"
+PFOCR_FLAVOR_ALL = "pending-pfocr_all"
+# PFOCR_FLAVOR_ALL = "pfocr_all_20231213_sdmo8dfz"
+PFOCR_FLAVOR_SYNONYMS = "pending-pfocr_synonyms"
+# PFOCR_FLAVOR_SYNONYMS = "pfocr_synonyms_20231213_sdmiqsdl"
+PFOCR_FLAVOR_STRICT = "pending-pfocr_strict"
+# PFOCR_FLAVOR_STRICT = "pfocr_strict_20231213_nfejmsas"
 
-ES_HOST = "localhost:9200"
+ES_HOST = "su12:9200"
 ES_INDEX = [PFOCR_FLAVOR_ALL, PFOCR_FLAVOR_SYNONYMS, PFOCR_FLAVOR_STRICT]
 ES_DOC_TYPE = "geneset"
 
 API_PREFIX = "pfocr"
 API_VERSION = ""
 
+ES_INDICES = copy.deepcopy(ES_INDICES)
+
+PFOCR_FLAVOR_INDICES = {
+    "strict": PFOCR_FLAVOR_STRICT,
+    "synonyms": PFOCR_FLAVOR_SYNONYMS,
+    "all": PFOCR_FLAVOR_ALL,
+}
+ES_INDICES.update(PFOCR_FLAVOR_INDICES)
+
 QUERY_KWARGS = copy.deepcopy(QUERY_KWARGS)
+
+FLAVOR_FILTERS = {
+    "strict": {"type": bool, "default": False},
+    "synonyms": {"type": bool, "default": False},
+    "all": {"type": bool, "default": False},
+}
+QUERY_KWARGS["*"].update(FLAVOR_FILTERS)
 QUERY_KWARGS["POST"].update({"minimum_should_match": {"type": int}, "operator": {"type": str}})
 
-ES_QUERY_BUILDER = "web.query_builders.PfocrQueryBuilder"
-# from biothings.api web/settings/default.py we have
-# ES_QUERY_PIPELINE = 'biothings.web.query.AsyncESQueryPipeline'
-# ES_QUERY_BUILDER = 'biothings.web.query.ESQueryBuilder'
-# ES_QUERY_BACKEND = 'biothings.web.query.AsyncESQueryBackend'
-# ES_RESULT_TRANSFORM = 'biothings.web.query.ESResultFormatter'
+# Default implementations: <from biothings.api web/settings/default.py>
+ES_QUERY_PIPELINE = "biothings.web.query.AsyncESQueryPipeline"
+ES_RESULT_TRANSFORM = "biothings.web.query.ESResultFormatter"
+ES_QUERY_BACKEND = "web.engine.pfocr.PFOCRBackend"
 
-APP_LIST = [
-    (r"/pfocr/{flavor}", "web.handlers.pfocr.PFOCR_Handler")
-]
+# Default: ES_QUERY_BUILDER = 'biothings.web.query.ESQueryBuilder'
+ES_QUERY_BUILDER = "web.query_builders.PfocrQueryBuilder"
