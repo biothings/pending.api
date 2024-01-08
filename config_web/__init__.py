e88c17918d63f4abfb52f43951a30b7b652bc1c4
Everaldo <everaldorodrigo@gmail.com>
2024-01-08 11:58:14 -0800

    Revert "Refactor opentelemetry (#166)" (#167)
    This reverts commit a74608c9a0ce0bc50b7d54366181e2a650c75e20.

--- config_web/__init__.py
+++ config_web/__init__.py
@@ -1,5 +1,3 @@
-# import os
-
 from . import agr
 from . import biggim  # use plugin "biggim_kp"
 from . import biggim_drugresponse_kp  # use plugin "BigGIM_Parser"
@@ -72,9 +70,3 @@ from . import ttd  # use plugin "BioThings_TTD_Dataplugin"
 from . import uberon
 from . import umlschem
 from . import upheno_ontology
-
-# Default Opentelemetry Settings
-OPENTELEMETRY_ENABLED = False
-OPENTELEMETRY_SERVICE_NAME = "Service Provider"
-OPENTELEMETRY_JAEGER_HOST = "localhost"
-OPENTELEMETRY_JAEGER_PORT = 6831
