1c628fbf9381722d37b0e009221bb1b73aaff4d0
everaldorodrigo <everaldorodrigo@gmail.com>
2024-01-25 16:05:14 -0800

    Update repoDB version.

--- plugins/repoDB/version.py
+++ plugins/repoDB/version.py
@@ -5,6 +5,6 @@ def get_release(self):
         https://github.com/biothings/biothings.api/blob/master/biothings/hub/dataload/dumper.py
     """
 
-    # repoDB version is "v2.0-SNAPSHOT (June 12, 2020)" as the code is written
+    # repoDB version is "v2.1-SNAPSHOT (June 15, 2023)"
     # See https://unmtid-shinyapps.net/shiny/repodb/#tab-2644-6
-    return "2.0"
+    return "2.1"
