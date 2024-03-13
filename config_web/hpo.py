0f806dd6092a188b6b83ca33bdeea60fa631fc65
jschaff <jschaff@scripps.edu>
2024-03-13 11:28:04 -0700

    Interim commit for the HPO configuration

--- config_web/hpo.py
+++ config_web/hpo.py
@@ -1,7 +1,8 @@
 import re
 
 # ES_HOST = "localhost:9200"
-ES_HOST = "es8.biothings.io:9200"
+# ES_HOST = "es8.biothings.io:9200"
+ES_HOST = "biothings.ci.transltr.io"
 ES_INDEX = "pending-hpo"
 ES_DOC_TYPE = "phenotype"
 
@@ -11,16 +12,26 @@ API_VERSION = ""
 
 # CURIE ID support based on BioLink Model
 BIOLINK_MODEL_PREFIX_BIOTHINGS_GENE_MAPPING = {
-    "HP": {"type": "phenotype", "field": "hp", "keep_prefix": True},
+    "HP": {
+        "type": "phenotype",
+        "field": "hp",
+        "regex_term_pattern": "(?P<term>HP:[0-9]+)",
+    }
 }
 biolink_curie_regex_list = []
 for (
     biolink_prefix,
     mapping,
 ) in BIOLINK_MODEL_PREFIX_BIOTHINGS_GENE_MAPPING.items():
-    expression = re.compile(rf"({biolink_prefix}):(?P<term>[^:]+)", re.I)
-    field_match = mapping["field"]
-    pattern = (expression, field_match)
+    field_match = mapping.get("field", [])
+    term_pattern = mapping.get("regex_term_pattern", None)
+    if term_pattern is None:
+        term_pattern = "(?P<term>[^:]+)"
+
+    raw_expression = rf"({biolink_prefix}):{term_pattern}"
+    compiled_expression = re.compile(raw_expression, re.I)
+
+    pattern = (compiled_expression, field_match)
     biolink_curie_regex_list.append(pattern)
 
 
