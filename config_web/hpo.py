dceba562d2d123dd0ae660ffb1004d05b8eebf4c
ctrl-schaff <59380780+ctrl-schaff@users.noreply.github.com>
2024-04-17 16:54:57 -0700

    Add phenotype curie id support (#186)
    * Add the initial biolink prefixes to the HPO plugin
    
    * Interim commit for the HPO configuration
    
    * Interim update for the HPO tests
    
    * Add default pattern for the _id queries
    
    * Simplify the HPO tests
    
    * Remove the unused import statements
    
    * Ignore assertion warnings from bandit in the tests ...
    
    See snippet below for directory skipping
    (biothings) jschaff@tsri-ubuntu:~/workspace/biothings/pending.api$ bandit tests/
    [main]  INFO    profile include tests: None
    [main]  INFO    profile exclude tests: None
    [main]  INFO    cli include tests: None
    [main]  INFO    cli exclude tests: None
    [main]  INFO    running on Python 3.10.12
    [manager]       WARNING Skipping directory (tests/), use -r flag to scan contents
    Run started:2024-04-17 23:36:32.207113
    
    Test results:
            No issues identified.
    
    * Add bandit specific configuration to ignore assert check
    
    ---------
    
    Co-authored-by: jschaff <jschaff@scripps.edu>

--- config_web/hpo.py
+++ config_web/hpo.py
@@ -1,6 +1,37 @@
+import re
+
 ES_HOST = "http://localhost:9200"
 ES_INDEX = "pending-hpo"
 ES_DOC_TYPE = "phenotype"
 
 API_PREFIX = "hpo"
 API_VERSION = ""
+
+
+# CURIE ID support based on BioLink Model
+BIOLINK_MODEL_PREFIX_BIOTHINGS_GENE_MAPPING = {
+    "HP": {"type": "phenotype", "field": "hp", "regex_term_pattern": "(?P<term>HP:[0-9]+)"}
+}
+biolink_curie_regex_list = []
+for (
+    biolink_prefix,
+    mapping,
+) in BIOLINK_MODEL_PREFIX_BIOTHINGS_GENE_MAPPING.items():
+    field_match = mapping.get("field", [])
+    term_pattern = mapping.get("regex_term_pattern", None)
+    if term_pattern is None:
+        term_pattern = "(?P<term>[^:]+)"
+
+    raw_expression = rf"({biolink_prefix}):{term_pattern}"
+    compiled_expression = re.compile(raw_expression, re.I)
+
+    pattern = (compiled_expression, field_match)
+    biolink_curie_regex_list.append(pattern)
+
+
+# id regex pattern
+id_hpo_regex_pattern = (re.compile(r"([\w]+):([0-9]+)", re.I), ["_id"])
+
+ANNOTATION_ID_REGEX_LIST = [*biolink_curie_regex_list, id_hpo_regex_pattern]
+
+ANNOTATION_DEFAULT_SCOPES = ["_id", "hp"]
