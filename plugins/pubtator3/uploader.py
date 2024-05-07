e2d04eb8b96a1356a95eda59e2390b1911f0abc3
jschaff <jschaff@scripps.edu>
2024-05-07 13:02:46 -0700

    Uploader structure change

--- plugins/pubtator3/uploader.py
+++ plugins/pubtator3/uploader.py
@@ -7,9 +7,14 @@ Handles the parsing of the documents provided by [relation2pubtator3.gz]
 import itertools
 from pathlib import Path
 from typing import Union
+import gzip
 
-import biothings, config
+import biothings
+from biothings import config
 import biothings.hub.dataload.uploader
+
+
+logger = config.logger
 
 
 class Pubtator3Uploader(biothings.hub.dataload.uploader.BaseSourceUploader):
@@ -22,8 +27,7 @@ class Pubtator3Uploader(biothings.hub.dataload.uploader.BaseSourceUploader):
         }
     }
 
-
-    def load_data(data_folder: Union[str, Path]):
+    def load_data(self, data_folder: Union[str, Path]):
         """
         We have multiple different instances for entries
 
@@ -37,110 +41,99 @@ class Pubtator3Uploader(biothings.hub.dataload.uploader.BaseSourceUploader):
         >>> 26018198	associate	ProteinMutation|RS#:121908192;HGVS:c.581G>A;CorrespondingGene:2671	ProteinMutation|RS#:771809901;HGVS:c.373C>T;CorrespondingGene:2671
         >>> 20817350        cause   Disease|MESH:D000544    ProteinMutation|RS#:1800562;CorrespondingGene:3077
         """
-        download_file_name = "relation2pubtator3"
+        download_file_name = "relation2pubtator3.gz"
         unstructured_relational_datafile = Path(data_folder).joinpath(download_file_name)
         unstructured_relational_datafile = unstructured_relational_datafile.resolve().absolute()
-        with open(unstructured_relational_datafile, "r", encoding="utf-8") as file_handle:
+        with gzip.open(unstructured_relational_datafile, "r", compresslevel=9) as file_handle:
             reading = True
             column_delimiter = "\t"
             entity_delimiter = "|"
             identifier_delimiter = ";"
             value_delimiter = ":"
+            chunksize = 10000
 
-            full_collection = set()
+            iteration_count = 0
             while reading:
-                raw_entry = file_handle.readline()
-                reading = raw_entry != ""
+                logger.info("Iteration #%s", iteration_count)
+                iteration_count += 1
+                chunk = file_handle.readlines(chunksize)
+                reading = len(chunk) > 0
 
-                entry_collection = raw_entry.strip().split(column_delimiter)
-                pmid = int(entry_collection[0])
-                predicate = str(entry_collection[1]).upper()
-                object_concept = str(entry_collection[2])
-                subject_concept = str(entry_collection[3])
+                for entry in chunk:
+                    entry_collection = entry.decode("utf-8").strip().split(column_delimiter)
+                    pmid = int(entry_collection[0])
+                    predicate = str(entry_collection[1]).upper()
+                    object_concept = str(entry_collection[2])
+                    subject_concept = str(entry_collection[3])
 
-                object_entity_type, __, object_id = object_concept.partition(entity_delimiter)
-                subject_entity_type, __, subject_id = subject_concept.partition(entity_delimiter)
+                    object_entity_type, __, object_id = object_concept.partition(entity_delimiter)
+                    subject_entity_type, __, subject_id = subject_concept.partition(entity_delimiter)
 
-                object_id_collection = object_id.split(identifier_delimiter)
-                subject_id_collection = subject_id.split(identifier_delimiter)
+                    object_id_collection = object_id.split(identifier_delimiter)
+                    subject_id_collection = subject_id.split(identifier_delimiter)
 
-                objects = []
-                object_values = []
-                subjects = []
-                subject_values = []
-                for object_id, subject_id in itertools.zip_longest(
-                    object_id_collection, subject_id_collection, fillvalue=None
-                ):
-                    if object_id is not None:
-                        sub_object_id = object_id.split(value_delimiter)
-                        match len(sub_object_id):
-                            case 1:
-                                object_identifer_key = None
-                                object_identifer_value = sub_object_id[0]
-                            case 2:
-                                object_identifer_key = sub_object_id[0]
-                                object_identifer_value = sub_object_id[1]
+                    objects = []
+                    object_values = []
+                    subjects = []
+                    subject_values = []
+                    for object_id, subject_id in itertools.zip_longest(
+                        object_id_collection, subject_id_collection, fillvalue=None
+                    ):
+                        if object_id is not None:
+                            sub_object_id = object_id.split(value_delimiter)
+                            match len(sub_object_id):
+                                case 1:
+                                    object_identifer_key = None
+                                    object_identifer_value = sub_object_id[0]
+                                case 2:
+                                    object_identifer_key = sub_object_id[0]
+                                    object_identifer_value = sub_object_id[1]
 
-                        objects.append(
-                            {
-                                "semantic_type_name": object_entity_type,
-                                "identifier": {
-                                    "key": object_identifer_key,
-                                    "value": object_identifer_value
+                            objects.append(
+                                {
+                                    "semantic_type_name": object_entity_type,
+                                    "identifier": {"key": object_identifer_key, "value": object_identifer_value},
                                 }
-                            }
-                        )
-                        object_values.append(object_identifer_value)
+                            )
+                            object_values.append(object_identifer_value)
 
-                    if subject_id is not None:
-                        sub_subject_id = subject_id.split(value_delimiter)
-                        match len(sub_subject_id):
-                            case 1:
-                                subject_identifier_key = None
-                                subject_identifier_value = sub_subject_id[0]
+                        if subject_id is not None:
+                            sub_subject_id = subject_id.split(value_delimiter)
+                            match len(sub_subject_id):
+                                case 1:
+                                    subject_identifier_key = None
+                                    subject_identifier_value = sub_subject_id[0]
 
-                            case 2:
-                                subject_identifier_key = sub_subject_id[0]
-                                subject_identifier_value = sub_subject_id[1]
+                                case 2:
+                                    subject_identifier_key = sub_subject_id[0]
+                                    subject_identifier_value = sub_subject_id[1]
 
-                        subjects.append(
-                            {
-                                "semantic_type_name": subject_entity_type,
-                                "identifier": {
-                                    "key": subject_identifier_key,
-                                    "value": subject_identifier_value
+                            subjects.append(
+                                {
+                                    "semantic_type_name": subject_entity_type,
+                                    "identifier": {"key": subject_identifier_key, "value": subject_identifier_value},
                                 }
-                            }
-                        )
-                        subject_values.append(subject_identifier_value)
+                            )
+                            subject_values.append(subject_identifier_value)
 
-                unique_id =(
-                    f"{pmid}"
-                    f"-{object_entity_type}"
-                    f"-{'|'.join(object_values)}"
-                    f"-{predicate}"
-                    f"-{subject_entity_type}"
-                    f"-{'|'.join(subject_values)}"
-                )
+                    unique_id = f"{pmid}-{object_concept}-{predicate}-{subject_concept}"
 
-                if len(objects) == 1:
-                    objects = objects[0]
-                if len(subjects) == 1:
-                    subjects = subjects[0]
+                    if len(objects) == 1:
+                        objects = objects[0]
+                    if len(subjects) == 1:
+                        subjects = subjects[0]
 
-                full_collection.add(unique_id)
-
-                document = {
-                    "_id": unique_id,
-                    "_version": 1,
-                    "object": objects,
-                    "pmid_count": 1,
-                    "predicate": predicate,
-                    "predication_count": 1,
-                    "pmid": pmid,
-                    "subject": subjects
-                }
-                yield document
+                    document = {
+                        "_id": unique_id,
+                        "_version": 1,
+                        "object": objects,
+                        "pmid_count": 1,
+                        "predicate": predicate,
+                        "predication_count": 1,
+                        "pmid": pmid,
+                        "subject": subjects,
+                    }
+                    yield document
 
     @classmethod
     def get_mapping(self) -> dict:
