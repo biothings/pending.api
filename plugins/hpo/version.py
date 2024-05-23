1081e0a463bac78b86074937d712e805906aef25
Everaldo <everaldorodrigo@gmail.com>
2024-05-23 16:40:51 -0700

    Upgrade biothings.api

--- plugins/hpo/version.py
+++ plugins/hpo/version.py
@@ -1,21 +1,12 @@
 def get_release(self):
-    import re
-    from datetime import datetime
 
-    dates = []
-    date_pattern = re.compile(r'\d{1,2}_\d{1,2}_\d{4}')
+    import datetime
+    import requests
 
-    for url in self.__class__.SRC_URLS:
-        res = self.client.head(url, allow_redirects=True)
-        header = res.headers['Content-Disposition']
-        match = date_pattern.findall(header)
-        if len(match) != 1:
-            raise ValueError("Parser Date Extraction Error")
-        dates.append(datetime.strptime(match[0], '%d_%m_%Y').date())
-
-    if dates[0] < dates[1]:
-        latest_version = dates[1]
-    else:
-        latest_version = dates[0]
-
-    return latest_version.isoformat()
+    res = requests.head('https://raw.githubusercontent.com/obophenotype/human-phenotype-ontology/master/hp.obo')
+    try:
+        return datetime.datetime.strptime(
+            res.headers['Date'], '%a, %d %b %Y %H:%M:%S %Z'
+        ).strftime('%Y-%m-%d')
+    except AttributeError:
+        return datetime.datetime.now().strftime('%Y-%m-%d')
