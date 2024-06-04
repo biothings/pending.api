ac06d4aa2063497f5d29a2d02975c29115c9ff1b
Dylan Welzel <dylanwelzel@gmail.com>
2024-06-04 10:15:12 -0700

    update config_web for ontologies

--- config_web/chebi.py
+++ config_web/chebi.py
@@ -1,6 +1,15 @@
+import copy
+
+from biothings.web.settings.default import QUERY_KWARGS
+
 ES_HOST = "http://localhost:9200"
 ES_INDEX = "pending-chebi"  # the index name convention is like "pending-<ApiName>"
 ES_DOC_TYPE = "chemical"
 
 API_PREFIX = "chebi"
 API_VERSION = ""
+
+_extra_kwargs = {"ignore_obsolete": {"type": bool, "default": True}}
+QUERY_KWARGS = copy.deepcopy(QUERY_KWARGS)
+QUERY_KWARGS["*"].update(_extra_kwargs)
+ES_QUERY_BUILDER = "web.query_builders.OntologyQueryBuilder"
--- config_web/ncit.py
+++ config_web/ncit.py
@@ -1,6 +1,15 @@
+import copy
+
+from biothings.web.settings.default import QUERY_KWARGS
+
 ES_HOST = "http://localhost:9200"
 ES_INDEX = "pending-ncit"
 ES_DOC_TYPE = "node"
 
 API_PREFIX = "ncit"
 API_VERSION = ""
+
+_extra_kwargs = {"ignore_obsolete": {"type": bool, "default": True}}
+QUERY_KWARGS = copy.deepcopy(QUERY_KWARGS)
+QUERY_KWARGS["*"].update(_extra_kwargs)
+ES_QUERY_BUILDER = "web.query_builders.OntologyQueryBuilder"
