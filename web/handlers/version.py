aad2069f9dfcdc2c29a58d04edbf4e3b290ed5de
Everaldo <everaldorodrigo@gmail.com>
2024-09-10 10:55:27 -0700

    Improve security in the version endpoint.

--- web/handlers/version.py
+++ web/handlers/version.py
@@ -12,9 +12,17 @@ class VersionHandler(BaseAPIHandler):
     def get_github_commit_hash(self):
         """Retrieve the current GitHub commit hash using gitpython."""
         try:
-            # Assuming the .git directory is in the parent folder of this file
-            repo_dir = os.path.abspath(os.path.dirname(__file__))
+            # Assuming the .git directory is located in the root of the project
+            repo_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))  # Adjust the path to reach the repo root
+            logger.info(f"Repository directory: {repo_dir}")
+
+            # Initialize the Repo object
             repo = Repo(repo_dir)
+
+            if repo.bare:
+                logger.error(f"Git repository not found in directory: {repo_dir}")
+                return "Unknown"
+
             commit_hash = repo.head.commit.hexsha  # Get the latest commit hash
             return commit_hash
         except Exception as e:
