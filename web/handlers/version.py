16240489b1f8a82a0b0055e4935e4a82dcc04146
Everaldo <everaldorodrigo@gmail.com>
2024-09-09 18:29:25 -0700

    Get github version.

--- web/handlers/version.py
+++ web/handlers/version.py
@@ -22,15 +22,5 @@ class VersionHandler(BaseAPIHandler):
             return "Unknown"
 
     async def get(self, *args, **kwargs):
-        # # Try to read the version from the version.txt file
-        # try:
-        #     with open("version.txt", "r") as version_file:
-        #         version = version_file.read().strip()
-        #         self.write(version)
-        # except FileNotFoundError:
-        #     # Handle the case where version.txt doesn't exist
-        #     logger.error("version.txt file not found")
-        #     self.set_status(404)
-        #     self.write("Version not available")
         version = self.get_github_commit_hash()
         self.write(version)
