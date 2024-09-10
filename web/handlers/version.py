3458e6456e5b97d114f7cb1608510dd8943dcf97
Everaldo <everaldorodrigo@gmail.com>
2024-09-10 11:53:31 -0700

    Improve path discover in the version endpoint.

--- web/handlers/version.py
+++ web/handlers/version.py
@@ -1,9 +1,7 @@
 import logging
-import os
 import pathlib
 import git
 
-from git import Repo
 from biothings.web.handlers import BaseAPIHandler
 
 logger = logging.getLogger(__name__)
@@ -20,15 +18,9 @@ class VersionHandler(BaseAPIHandler):
             # Use git.Repo to find the root of the repository
             repo = git.Repo(file_path, search_parent_directories=True)
 
-            # Get the absolute path to the repository root
-            repo_dir = repo.working_tree_dir
-
-            logger.info(f"Repository directory: {repo_dir}")
-
-            # Initialize the Repo object
-            repo = Repo(repo_dir)
-
             if repo.bare:
+                # Get the absolute path to the repository root
+                repo_dir = repo.working_tree_dir
                 logger.error(f"Git repository not found in directory: {repo_dir}")
                 return "Unknown"
 
