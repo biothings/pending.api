bf23dc4254bab7f72ff3e99bae474aee7652638b
Everaldo <everaldorodrigo@gmail.com>
2024-09-23 11:08:44 -0700

    Redirect status to rhea. (#239)
    * Redirect status to rhea.
    
    * Refactor status endpoint to avoid caching and access rhea endpoint.
    
    * Call rhea handler directly.

--- web/handlers/status.py
+++ web/handlers/status.py
@@ -1,40 +1,34 @@
 import logging
-import os
-import json
 
 from biothings.web.handlers import BaseAPIHandler
-from tornado.web import HTTPError
-from tornado.httpclient import AsyncHTTPClient, HTTPError as TornadoHTTPError
+from biothings.web.handlers.services import StatusHandler
 
 
 logger = logging.getLogger(__name__)
 
 
-class StatusHandler(BaseAPIHandler):
+class StatusDefaultHandler(BaseAPIHandler):
     name = "status"
 
+    def set_default_headers(self):
+        """Set headers to prevent caching."""
+        self.set_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.set_header("Pragma", "no-cache")
+
     async def get(self, *args, **kwargs):
-        http_client = AsyncHTTPClient()
-        host = str(os.getenv("ES_HOST", "http://localhost:9200"))
-        url_check = f"{host}/pending-rhea/_count"
-        logger.info(f"url_check: {url_check}")
+        self.set_default_headers()
+        self.request.uri = "/rhea/status"
+        status_handler = StatusHandler(
+            self.application, self.request, **kwargs
+        )
+        status_handler._transforms = self._transforms  # Copy transforms from current handler
+        await status_handler.get()
 
-        try:
-            response = await http_client.fetch(url_check)
-            response_data = json.loads(response.body.decode())
-
-            if response.code == 200:
-                count = response_data.get('count', 0)
-
-                if count > 0:
-                    self.write({"success": True})
-                else:
-                    logger.error(f"Error fetching _count from Elasticsearch: {response.body.decode()}")
-                    raise HTTPError(response.code, f"Error fetching _count: {response.body.decode()}")
-            else:
-                logger.error(f"Received non-200 response from Elasticsearch _count endpoint: {response.code}")
-                raise HTTPError(response.code, f"Error fetching _count: {response.body.decode()}")
-
-        except TornadoHTTPError as e:
-            logger.error(f"Error fetching _count from Elasticsearch: {e}")
-            raise HTTPError(e.code, f"Error fetching _count: {e.message}")
+    async def head(self, *args, **kwargs):
+        self.set_default_headers()
+        self.request.uri = "/rhea/status"
+        status_handler = StatusHandler(
+            self.application, self.request, **kwargs
+        )
+        status_handler._transforms = self._transforms  # Copy transforms from current handler
+        await status_handler.head()
