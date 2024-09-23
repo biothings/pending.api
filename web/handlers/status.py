f65ff2912cc998dd0f65a416f69227957d1126e4
Everaldo <everaldorodrigo@gmail.com>
2024-09-23 11:53:39 -0700

    Fix duplicated logs for status handler. (#240)
    * Fix duplicated logs for status handler.
    
    * Keep default headers.

--- web/handlers/status.py
+++ web/handlers/status.py
@@ -10,25 +10,19 @@ logger = logging.getLogger(__name__)
 class StatusDefaultHandler(BaseAPIHandler):
     name = "status"
 
-    def set_default_headers(self):
-        """Set headers to prevent caching."""
-        self.set_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
-        self.set_header("Pragma", "no-cache")
-
     async def get(self, *args, **kwargs):
-        self.set_default_headers()
         self.request.uri = "/rhea/status"
         status_handler = StatusHandler(
             self.application, self.request, **kwargs
         )
-        status_handler._transforms = self._transforms  # Copy transforms from current handler
-        await status_handler.get()
+        await status_handler._execute([])
+        self._finished = True  # Ensure the request handling is marked as complete
+
 
     async def head(self, *args, **kwargs):
-        self.set_default_headers()
         self.request.uri = "/rhea/status"
         status_handler = StatusHandler(
             self.application, self.request, **kwargs
         )
-        status_handler._transforms = self._transforms  # Copy transforms from current handler
-        await status_handler.head()
+        await status_handler._execute([])
+        self._finished = True  # Ensure the request handling is marked as complete
