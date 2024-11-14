284ac870b72addcb3826460cba5eaaad88f9cb7c
Everaldo <everaldorodrigo@gmail.com>
2024-11-14 12:18:05 -0800

    Redirect DISEASE to disease.

--- web/handlers/diseases.py
+++ web/handlers/diseases.py
@@ -4,8 +4,10 @@ from biothings.web.handlers import BaseAPIHandler
 class DiseasesHandler(BaseAPIHandler):
     name = "diseases"
 
-    async def get(self, *args, **kwargs):
-        self.redirect("/diseases", permanent=True)
-
-    async def head(self, *args, **kwargs):
-        self.redirect("/diseases", permanent=True)
+    def prepare(self):
+        current_path = self.request.path
+        if current_path.startswith("/DISEASES"):
+            new_path = current_path.replace("/DISEASES", "/diseases", 1)
+            if self.request.query:
+                new_path += "?" + self.request.query
+            self.redirect(new_path, status=308)
