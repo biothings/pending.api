6bb66230cae43ccb40caaa1c004359433036611d
Marco Cano <artofmarco@gmail.com>
2025-01-13 12:39:57 -0800

    feat: :sparkles: updated UI

--- web/handlers/__init__.py
+++ web/handlers/__init__.py
@@ -11,6 +11,7 @@ import tornado.httpclient
 import tornado.web
 from biothings.web.handlers import BaseHandler
 from jinja2 import Environment, FileSystemLoader
+from config_web import (OPENTELEMETRY_ENABLED)
 
 # from config_web import opentelemetry
 from .annotator import AnnotatorHandler
@@ -21,47 +22,14 @@ from .ngd import SemmedNGDHandler
 from .status import StatusDefaultHandler
 from .version import VersionHandler
 
-from config_web import (
-    OPENTELEMETRY_ENABLED,
-    OPENTELEMETRY_JAEGER_HOST,
-    OPENTELEMETRY_JAEGER_PORT,
-    OPENTELEMETRY_SERVICE_NAME,
-)
-
-OPENTELEMETRY_ENABLED = os.getenv("OPENTELEMETRY_ENABLED", OPENTELEMETRY_ENABLED).lower()
-
-if OPENTELEMETRY_ENABLED == "true":
-    OPENTELEMETRY_JAEGER_HOST = os.getenv("OPENTELEMETRY_JAEGER_HOST", OPENTELEMETRY_JAEGER_HOST)
-    OPENTELEMETRY_JAEGER_PORT = int(os.getenv("OPENTELEMETRY_JAEGER_PORT", OPENTELEMETRY_JAEGER_PORT))
-    OPENTELEMETRY_SERVICE_NAME = os.getenv("OPENTELEMETRY_SERVICE_NAME", OPENTELEMETRY_SERVICE_NAME)
-
-    from opentelemetry.instrumentation.tornado import TornadoInstrumentor
-
-    TornadoInstrumentor().instrument()
-
-    # Configure the OpenTelemetry exporter
-    from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-    from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-    from opentelemetry.sdk.trace import TracerProvider
-    from opentelemetry.sdk.trace.export import BatchSpanProcessor
-    from opentelemetry import trace
-
-    trace_exporter = JaegerExporter(
-        agent_host_name=OPENTELEMETRY_JAEGER_HOST,
-        agent_port=OPENTELEMETRY_JAEGER_PORT,
-        udp_split_oversized_batches=True,
-    )
-
-    trace_provider = TracerProvider(resource=Resource.create({SERVICE_NAME: OPENTELEMETRY_SERVICE_NAME}))
-    trace_provider.add_span_processor(BatchSpanProcessor(trace_exporter))
-
-    # Set the trace provider globally
-    trace.set_tracer_provider(trace_provider)
-
 log = logging.getLogger("pending")
 
 templateLoader = FileSystemLoader(searchpath="web/templates/")
 templateEnv = Environment(loader=templateLoader, cache_size=0)
+
+OPENTELEMETRY_ENABLED = os.getenv("OPENTELEMETRY_ENABLED", OPENTELEMETRY_ENABLED).lower()
+if OPENTELEMETRY_ENABLED == "true":
+    Observability()
 
 
 def hostname_to_site(hostname: str) -> str:
@@ -79,8 +47,18 @@ def hostname_to_site(hostname: str) -> str:
 
     return "pending"
 
+class WebBaseHandler(BaseHandler):
+    def get_api_list(self):
+        """
+        Generate the API list from the biothings configuration.
+        """
+        root = self.biothings.config._primary
+        attrs = [getattr(root, attr) for attr in dir(root)]
+        confs = [attr for attr in attrs if isinstance(attr, types.ModuleType)]
+        return [{"_id": conf.API_PREFIX, "status": "running"} for conf in confs]
 
-class FrontPageHandler(BaseHandler):
+
+class FrontPageHandler(WebBaseHandler):
 
     # Cache the template output
     cached_template_output = {}
@@ -101,10 +79,7 @@ class FrontPageHandler(BaseHandler):
         if FrontPageHandler.cached_template_output.get(site, False):
             return FrontPageHandler.cached_template_output[site]
 
-        root = self.biothings.config._primary
-        attrs = [getattr(root, attr) for attr in dir(root)]
-        confs = [attr for attr in attrs if isinstance(attr, types.ModuleType)]
-        apilist = [{"_id": conf.API_PREFIX, "status": "running"} for conf in confs]
+        apilist = self.get_api_list()  # Get the API list
 
         templateEnv.globals["site"] = site
         template = templateEnv.get_template("index.html")
@@ -128,15 +103,16 @@ class FrontPageHandler(BaseHandler):
         self.finish()
 
 
-class ApiViewHandler(tornado.web.RequestHandler):
+class ApiViewHandler(WebBaseHandler):
     def get(self):
         # templateEnv.globals['site'] = "pending"
         # if self.request.host == "biothings.ncats.io":
         #     templateEnv.globals['site'] = "ncats"
+        apilist = self.get_api_list()  # Get the API list
 
         templateEnv.globals["site"] = hostname_to_site(self.request.host)
         template = templateEnv.get_template("try.html")
-        output = template.render()
+        output = template.render(Context=json.dumps({"List": apilist}))
         self.finish(output)
 
 
