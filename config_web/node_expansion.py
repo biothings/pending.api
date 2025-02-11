7432b19efef173b4672cd7fd5c100c9ea5e7f2b4
Everaldo <everaldorodrigo@gmail.com>
2025-02-10 16:06:52 -0800

    Add ES host to node_expansion. (#271)

--- config_web/node_expansion.py
+++ config_web/node_expansion.py
@@ -1,5 +1,6 @@
 ES_INDEX = ["pending-go", "pending-doid", "pending-mondo", "pending-chebi"]
 ES_DOC_TYPE = "node"
 
+ES_HOST = "http://localhost:9200"
 API_PREFIX = "node-expansion"
 API_VERSION = ""
