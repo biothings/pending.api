9be9a237c1ef838cf0261a421675d6d04ddcee20
Colleen Xu <colleenhxu@gmail.com>
2025-03-26 15:20:58 -0700

    EBI gene2pheno: refactored parser files
    need to update source of data files: Johnathan will help with this
    creates association-based documents

--- plugins/ebi_gene2phenotype/parser.py
+++ plugins/ebi_gene2phenotype/parser.py
@@ -1,90 +1,190 @@
-import csv
-import io
-import json
-import glob
-import sys
-import os.path
-from datetime import datetime
+import pathlib
+import pandas as pd
 
 
-def load_data(data_folder):
-    dt1 = load_csv(data_folder, "CancerG2P_*_*_*.csv")
-    dt2 = load_csv(data_folder, "DDG2P_*_*_*.csv")
+def _load_multiple_csv_into_one_df(folder_path: pathlib.Path, data_file_pattern: str) -> pd.DataFrame:
+    """
+    folder_path: pathlib Path to folder containing csv files
+    data_file_pattern: str pattern for csv file pathnames (syntax: pathlib's pattern language)
 
-    props_names = clean_headers(dt1[0])
-    dt1_cleaned = clean_csv_data(dt1[1:])
-    dt2_cleaned = clean_csv_data(dt2[1:])
+    output: pandas dataframe containing data from multiple csv files
 
-    dt = dt1_cleaned + dt2_cleaned
-    result_dict = parse_data(dt, props_names)
+    Assumptions:
+    * All data files are csv (can be compressed) AND have the same header so they can be concatenated easily.
+      pandas can infer and automatically do on-the-fly decompression for some file extensions
+      (see pandas.read_csv for details)
+    * It's fine to set all column types to str (one exception: ingesting "date of last review"
+      as datetime to save memory)
+    """
+    ## using pathlib's Path.glob method, which produces a generator
+    ## then turning the generator into a list, so can check if it's empty
 
-    for v in result_dict.values():
-        yield v
+    all_file_paths = list(folder_path.glob(data_file_pattern))
 
-def load_csv(data_folder, pattern):
-    files = glob.glob(os.path.join(data_folder, pattern))
-    if files:
-        with open(files[0], 'r') as f:
-            return list(csv.reader(f))
+    ## read_csv dtype notes:
+    ## - Want to read "gene mim" and "disease mim" columns as str. Otherwise, default reading will be float
+    ##   for some files (extra steps to handle)
+    ## - All IDs in final API docs should be strings (ex: "publications")
+    ## - saves memory to read "date of last review" as datetime
+    ## - didn't save memory to read some columns as "category"
+
+    ## using generator expression (think list/dict comprehension) within pd.concat to load files 1 at a time
+    if all_file_paths:
+        return pd.concat(
+            (pd.read_csv(f, dtype=str, parse_dates=["date of last review"]) for f in all_file_paths),
+            ignore_index=True,
+        )
     else:
-        raise FileNotFoundError(f"Can't find input file matching pattern {pattern}")
-
-def clean_headers(headers):
-    return [name.replace(' ', '_') for name in headers]
-
-def clean_csv_data(rows):
-    for row in rows:
-        if row[1] == 'No gene mim':
-            row[1] = ''
-        if row[3] == 'No disease mim':
-            row[3] = ''
-    return rows
-
-def parse_data(data, headers):
-    result_dict = {}
-    for row in data:
-        dict_gene = parse_row(row, headers)
-        id_key = int(row[12])
-        if id_key in result_dict:
-            result_dict[id_key]['gene2phenotype'].append(dict_gene)
-        else:
-            result_dict[id_key] = {
-                "_id": row[12],
-                "gene2phenotype": [dict_gene]
-            }
-    return result_dict
-
-def parse_row(row, headers):
-    dict_gene = {}
-    for y, cell in enumerate(row):
-        if cell != '':
-            if y in {2, 3}:
-                if 'disease' not in dict_gene:
-                    dict_gene['disease'] = {}
-                dict_gene['disease'][headers[y]] = cell
-            elif y in {7, 8, 11}:
-                dict_gene[headers[y]] = cell.split(';')
-            elif y == 9:
-                dict_gene[headers[y]] = [int(x) for x in cell.split(';')]
-            elif y == 13:
-                if cell != 'No date':
-                    dict_gene[headers[y]] = datetime.strptime(cell, '%Y-%m-%d %H:%M:%S').isoformat()
-            elif y != 12:
-                dict_gene[headers[y]] = cell
-    return dict_gene
+        raise FileNotFoundError(f"Can't find files in `{folder_path}` matching `{data_file_pattern}`")
 
 
-if __name__ == "__main__":
+def duplicates_check(dataframe: pd.DataFrame, column_subset: list[str]):
+    """
+    dataframe: pandas dataframe
+    column_subset: array of column names in dataframe that should uniquely define one record/row
+    
+    Returns nothing.
 
-    if (len(sys.argv) != 2):
-        print('python parser.py <output_file>')
-        exit(1)
+    This function checks whether drop_duplicates using all columns will actually remove all duplicates.
+    Many column values are delimited strings, and my concern is that these could differ only in list order
+    for the same records in different files/panels.
 
-    result_list = []
-    for r in load_data(os.getcwd()):
-        result_list.append(r)
+    If the deplicated data with this column subset == duplicated data using all columns:
+      Then this column subset does uniquely define records / rows.
+      And drop_duplicates using all columns should work as expected.
+    Else: raise AssertionError
+      The other columns of the data need more investigation (something else is contributing to the
+      uniqueness of each row, and it could be delimited-string list order).
+      And the parser probably needs adjustment.
+    """
 
-    # save to a file
-    file = io.open(sys.argv[1], "w", encoding='utf8')
-    file.write(json.dumps(result_list, indent=4, sort_keys=True, default=str))
-    file.close()
+    n_duplicates_subset = dataframe[dataframe.duplicated(subset=column_subset, keep=False)].shape
+    n_duplicates_all = dataframe[dataframe.duplicated(keep=False)].shape
+
+    if n_duplicates_subset != n_duplicates_all:
+        raise AssertionError(
+            "The data format has changed, and record de-duplication may not work as-expected. "
+            "Double-check the data and what columns uniquely define one record"
+        )
+
+
+## main function can have any name.
+## Put into manifest's uploader.parser field: "parser:{funct name}"
+## DON'T NEED A MAIN EXECUTION BLOCK `if __name__ == "__main__"`
+
+
+def upload_documents(data_folder: str):
+    """
+    data_folder: Biothings will provide the data_folder (path) when running this function
+
+    output: yields document from 1 row at a time
+    """
+
+    ## LOAD DATA
+    ## doing resolve() just in case
+    base_file_path = pathlib.Path(data_folder).resolve()
+    ## using "*.csv.gz": data files from FTP site have this extension. And want all data files/panels
+    df = _load_multiple_csv_into_one_df(folder_path=base_file_path, data_file_pattern="*.csv.gz")
+    ## make column names snake-case to make them usable with itertuples later
+    df.columns = df.columns.str.replace(" ", "_")
+
+    ## DROP DUPLICATES
+    ## First check that drop_duplicates using all columns will work as-intended, raise error if concern
+    ## Based on exploring the data, this column subset should uniquely define one record/row
+    ##   (none have delimited strings).
+    duplicates_check(df, ["g2p_id", "gene_symbol", "disease_name", "allelic_requirement", "molecular_mechanism"])
+    ## drop duplicates if no error was raised
+    df.drop_duplicates(inplace=True, ignore_index=True)
+
+    ## COLUMN-LEVEL TRANSFORMS
+    ## 1. adding Translator/biolink prefixes to IDs
+    df["gene_mim"] = "OMIM:" + df["gene_mim"]
+    df["hgnc_id"] = "HGNC:" + df["hgnc_id"]
+    df["disease_mim"] = df["disease_mim"].str.replace("Orphanet", "orphanet")
+    ## done to preserve NA, orphanet values in column
+    df["disease_mim"] = [i if pd.isna(i)
+                         else "OMIM:" + i if i.isnumeric()
+                         else i 
+                         for i in df["disease_mim"]]
+    ## 2. strip whitespace
+    df["disease_name"] = df["disease_name"].str.strip()
+    df["comments"] = df["comments"].str.strip()
+    ## 3. create new columns
+    ## UI really wants resource website urls like this. May need to adjust over time as website changes
+    df["g2p_record_url"] = "https://www.ebi.ac.uk/gene2phenotype/lgd/" + df["g2p_id"]
+    ## 4. replace panel keywords with full human-readable names (based on what's shown on G2P website)
+    ## keeping "Hearing loss" as-is, changing all other values
+    df["panel"] = df["panel"].str.replace("DD", "Developmental disorders")
+    df["panel"] = df["panel"].str.replace("Cancer", "Cancer disorders")
+    df["panel"] = df["panel"].str.replace("Cardiac", "Cardiac disorders")
+    df["panel"] = df["panel"].str.replace("Eye", "Eye disorders")
+    df["panel"] = df["panel"].str.replace("Skeletal", "Skeletal disorders")
+    df["panel"] = df["panel"].str.replace("Skin", "Skin disorders")
+
+    ## GENERATING DOCS
+    ## using itertuples because it's faster, preserves column datatypes
+    for row in df.itertuples(index=False):
+        ## 1. simple assignments
+        document = {
+            "_id": row.g2p_id,
+            "subject": {
+                "hgnc_symbol": row.gene_symbol, 
+                "hgnc": row.hgnc_id, 
+                "type": "Gene"
+            },
+            "association": {
+                "g2p_record_id": row.g2p_id,
+                "g2p_record_url": row.g2p_record_url,
+                "allelic_requirement": row.allelic_requirement,
+                "confidence": row.confidence,
+                "molecular_mechanism": row.molecular_mechanism,
+                "molecular_mechanism_categorisation": row.molecular_mechanism_categorisation,
+                "g2p_panels": [i.strip() for i in row.panel.split(";")],
+                ## cast into str for export
+                "date_of_last_review": str(row.date_of_last_review),
+            },
+            "object": {
+                "name": row.disease_name, 
+                "type": "Disease"
+            },
+        }
+
+        ## 2. only create field if value is not NA. list comprehension with split won't work if value is NA
+        ## 2A. Gene
+        if pd.notna(row.gene_mim):
+            document["subject"]["omim"] = row.gene_mim
+        if pd.notna(row.previous_gene_symbols):
+            document["subject"]["previous_gene_symbols"] = [i.strip() for i in row.previous_gene_symbols.split(";")]
+
+        ## 2B. Association
+        if pd.notna(row.cross_cutting_modifier):
+            document["association"]["cross_cutting_modifiers"] = [
+                i.strip() for i in row.cross_cutting_modifier.split(";")
+            ]
+        if pd.notna(row.variant_consequence):
+            document["association"]["variant_consequences"] = [i.strip() for i in row.variant_consequence.split(";")]
+        if pd.notna(row.variant_types):
+            document["association"]["variant_types"] = [i.strip() for i in row.variant_types.split(";")]
+        ## uses diff delimiters, could do more parsing
+        if pd.notna(row.molecular_mechanism_evidence):
+            document["association"]["molecular_mechanism_evidence"] = [
+                i.strip() for i in row.molecular_mechanism_evidence.split("&")
+            ]
+        if pd.notna(row.phenotypes):
+            document["association"]["phenotypes"] = [i.strip() for i in row.phenotypes.split(";")]
+        if pd.notna(row.publications):
+            document["association"]["pmids"] = [i.strip() for i in row.publications.split(";")]
+        if pd.notna(row.comments):
+            document["association"]["curator_comments"] = row.comments
+
+        ## 2C. Disease
+        ## disease_mim: create field depending on whether OMIM or orphanet
+        if pd.notna(row.disease_mim):
+            if row.disease_mim.startswith("orphanet"):
+                document["object"]["orphanet"] = row.disease_mim
+            elif row.disease_mim.startswith("OMIM"):
+                document["object"]["omim"] = row.disease_mim
+        if pd.notna(row.disease_MONDO):
+            document["object"]["mondo"] = row.disease_MONDO
+
+        yield document
