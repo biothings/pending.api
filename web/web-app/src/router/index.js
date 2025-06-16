149b340b1f6cb3830ac9f973db47b3cd77d75cca
Everaldo <everaldorodrigo@gmail.com>
2025-06-16 12:59:34 -0700

    Fix route when ending in slash /.

--- web/web-app/src/router/index.js
+++ web/web-app/src/router/index.js
@@ -56,13 +56,4 @@ const router = createRouter({
   ],
 })
 
-router.beforeEach((to, from, next) => {
-  const path = to.path
-  if (path !== '/' && path.endsWith('/')) {
-    next({ path: path.slice(0, -1), query: to.query, hash: to.hash })
-  } else {
-    next()
-  }
-})
-
 export default router
