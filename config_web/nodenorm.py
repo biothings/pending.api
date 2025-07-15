68718129b4a8dd028246971c0b8f19e103071b91
ctrl-schaff <schaffjohnathan@gmail.com>
2025-07-15 11:57:18 -0500

    Update the config-web with the rtx-kg2 plugin

--- config_web/nodenorm.py
+++ config_web/nodenorm.py
@@ -1,3 +1,5 @@
+from biothings.web.settings.default import APP_LIST
+
 ES_HOST = "http://localhost:9200"
 ES_INDEX = "pending-nodenorm"
 ES_DOC_TYPE = "node"
@@ -5,3 +7,10 @@ ES_DOC_TYPE = "node"
 API_PREFIX = "nodenorm"
 API_VERSION = ""
 API_DEPRECATED = False
+
+
+APP_LIST = [(r"/{pre}/{ver}/query/graph?", "web.handlers.GraphQueryHandler"), *APP_LIST]
+
+ES_QUERY_PIPELINE = "web.pipeline.PendingQueryPipeline"
+ES_QUERY_BUILDER = "web.pipeline.PendingQueryBuilder"
+ES_RESULT_TRANSFORM = "web.pipeline.GraphResultTransform"
