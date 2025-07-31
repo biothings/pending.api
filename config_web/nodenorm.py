bec6bcfd8bd22be2a4ac819ef3ba23b14da98644
ctrl-schaff <schaffjohnathan@gmail.com>
2025-07-31 16:06:09 -0500

    Add the new endpoint to the config_web API_LIST

--- config_web/nodenorm.py
+++ config_web/nodenorm.py
@@ -1,6 +1,10 @@
+from biothings.web.settings.default import APP_LIST
+
 ES_HOST = "http://localhost:9200"
 ES_INDEX = "pending-nodenorm"
 ES_DOC_TYPE = "node"
+
+APP_LIST = [(r"/{pre}/{ver}/get_normalized_nodes?", "web.handlers.NormalizedNodesHandler"), *APP_LIST]
 
 API_PREFIX = "nodenorm"
 API_VERSION = ""
