164332074f80b0fa1ab39da6941547cc2cf2abcf
ctrl-schaff <schaffjohnathan@gmail.com>
2025-08-23 20:26:36 -0500

    Update the nodenorm handler tests

--- tests/handlers/test_nodenorm.py
+++ tests/handlers/test_nodenorm.py
@@ -38,7 +38,7 @@ class TestNodeNormalizationHandler(AsyncHTTPTestCase):
         """
         normalized_nodes_endpoint = r"/nodenorm/get_normalized_nodes"
         url = self.get_url(normalized_nodes_endpoint)
-        body = json.dumps({ "curie": [ "MESH:D014867", "NCIT:C34373"] })
+        body = json.dumps({"curie": ["MESH:D014867", "NCIT:C34373"]})
         headers = {"Content-Type": "application/json"}
 
         http_client = AsyncHTTPClient()
@@ -47,50 +47,61 @@ class TestNodeNormalizationHandler(AsyncHTTPTestCase):
 
         expected_body = {
             "MESH:D014867": {
-                "id": {
-                    "identifier": "CHEBI:15377"
-                },
                 "equivalent_identifiers": [
                     {
-                        "identifier": "CHEBI:15377"
+                        "identifier": "CHEBI:15377",
+                        "label": "water",
                     },
                     {
-                        "identifier": "UNII:059QF0KO0R"
+                        "identifier": "UNII:059QF0KO0R",
+                        "label": "WATER",
                     },
                     {
-                        "identifier": "PUBCHEM.COMPOUND:962"
+                        "identifier": "PUBCHEM.COMPOUND:962",
+                        "label": "Water",
                     },
                     {
-                        "identifier": "CHEMBL.COMPOUND:CHEMBL1098659"
+                        "identifier": "CHEMBL.COMPOUND:CHEMBL1098659",
+                        "label": "WATER",
                     },
                     {
-                        "identifier": "DRUGBANK:DB09145"
+                        "identifier": "DRUGBANK:DB09145",
+                        "label": "Water",
                     },
                     {
-                        "identifier": "MESH:D014867"
+                        "identifier": "MESH:D014867",
+                        "label": "Water",
                     },
                     {
-                        "identifier": "CAS:231-791-2"
+                        "identifier": "CAS:231-791-2",
                     },
                     {
-                        "identifier": "CAS:7732-18-5"
+                        "identifier": "CAS:7732-18-5",
                     },
                     {
-                        "identifier": "HMDB:HMDB0002111"
+                        "identifier": "HMDB:HMDB0002111",
+                        "label": "Water",
                     },
                     {
-                        "identifier": "KEGG.COMPOUND:C00001"
+                        "identifier": "KEGG.COMPOUND:C00001",
+                        "label": "H2O",
                     },
                     {
-                        "identifier": "INCHIKEY:XLYOFNOQVPJJNP-UHFFFAOYSA-N"
+                        "identifier": "INCHIKEY:XLYOFNOQVPJJNP-UHFFFAOYSA-N",
                     },
                     {
-                        "identifier": "UMLS:C0043047"
+                        "identifier": "UMLS:C0043047",
+                        "label": "water",
                     },
                     {
-                        "identifier": "RXCUI:11295"
-                    }
+                        "identifier": "RXCUI:11295",
+                    },
                 ],
+                "id": {
+                    "identifier": "CHEBI:15377",
+                    "label": "Water",
+                },
+                "information_content": 47.7,
                 "type": [
                     "biolink:SmallMolecule",
                     "biolink:MolecularEntity",
@@ -100,69 +111,75 @@ class TestNodeNormalizationHandler(AsyncHTTPTestCase):
                     "biolink:ChemicalEntityOrGeneOrGeneProduct",
                     "biolink:ChemicalEntityOrProteinOrPolypeptide",
                     "biolink:NamedThing",
-                    "biolink:PhysicalEssenceOrOccurrent"
+                    "biolink:PhysicalEssenceOrOccurrent",
                 ],
-                "information_content": 47.7
             },
             "NCIT:C34373": {
-                "id": {
-                    "identifier": "MONDO:0004976"
-                },
                 "equivalent_identifiers": [
                     {
-                        "identifier": "MONDO:0004976"
+                        "identifier": "MONDO:0004976",
+                        "label": "amyotrophic lateral sclerosis",
                     },
                     {
-                        "identifier": "DOID:332"
+                        "identifier": "DOID:332",
+                        "label": "amyotrophic lateral sclerosis",
                     },
                     {
-                        "identifier": "orphanet:803"
+                        "identifier": "orphanet:803",
                     },
                     {
-                        "identifier": "UMLS:C0002736"
+                        "identifier": "UMLS:C0002736",
+                        "label": "Amyotrophic Lateral Sclerosis",
                     },
                     {
-                        "identifier": "MESH:D000690"
+                        "identifier": "MESH:D000690",
+                        "label": "Amyotrophic Lateral Sclerosis",
                     },
                     {
-                        "identifier": "MEDDRA:10002026"
+                        "identifier": "MEDDRA:10002026",
                     },
                     {
-                        "identifier": "MEDDRA:10052889"
+                        "identifier": "MEDDRA:10052889",
                     },
                     {
-                        "identifier": "NCIT:C34373"
+                        "identifier": "NCIT:C34373",
+                        "label": "Amyotrophic Lateral Sclerosis",
                     },
                     {
-                        "identifier": "SNOMEDCT:86044005"
+                        "identifier": "SNOMEDCT:86044005",
                     },
                     {
-                        "identifier": "medgen:274"
+                        "identifier": "medgen:274",
                     },
                     {
-                        "identifier": "icd11.foundation:1982355687"
+                        "identifier": "icd11.foundation:1982355687",
                     },
                     {
-                        "identifier": "ICD10:G12.21"
+                        "identifier": "ICD10:G12.21",
                     },
                     {
-                        "identifier": "ICD9:335.20"
+                        "identifier": "ICD9:335.20",
                     },
                     {
-                        "identifier": "KEGG.DISEASE:05014"
+                        "identifier": "KEGG.DISEASE:05014",
                     },
                     {
-                        "identifier": "HP:0007354"
-                    }
+                        "identifier": "HP:0007354",
+                        "label": "Amyotrophic lateral sclerosis",
+                    },
                 ],
+                "id": {
+                    "identifier": "MONDO:0004976",
+                    "label": "amyotrophic lateral sclerosis",
+                },
+                "information_content": 74.9,
                 "type": [
                     "biolink:Disease",
                     "biolink:DiseaseOrPhenotypicFeature",
                     "biolink:BiologicalEntity",
                     "biolink:ThingWithTaxon",
-                    "biolink:NamedThing"
+                    "biolink:NamedThing",
                 ],
-                "information_content": 74.9
-            }
+            },
         }
         assert expected_body == body
