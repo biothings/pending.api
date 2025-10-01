5ce0941ca6fc29949ce0951d4a20547e8b822200
ctrl-schaff <schaffjohnathan@gmail.com>
2025-10-01 13:45:00 -0500

    Update the config_web nodenorm entry

--- config_web/nodenorm.py
+++ config_web/nodenorm.py
@@ -1,12 +1,15 @@
 from biothings.web.settings.default import APP_LIST
 
+from web.handlers.nodenorm import NormalizedNodesHandler, SetIdentifierHandler
+
 ES_HOST = "http://localhost:9200"
 ES_INDEX = "pending-nodenorm"
 ES_DOC_TYPE = "node"
 
 APP_LIST = [
-    (r"/{pre}/{ver}/get_normalized_nodes?", "web.handlers.nodenorm.normalized_nodes.NormalizedNodesHandler"), *APP_LIST,
-    (r"/{pre}/{ver}/get_setid?", "web.handlers.nodenorm.set_identifiers"), *APP_LIST
+    (r"/{pre}/{ver}/get_normalized_nodes?", NormalizedNodesHandler),
+    (r"/{pre}/{ver}/get_setid?", SetIdentifierHandler),
+    *APP_LIST,
 ]
 
 API_PREFIX = "nodenorm"
