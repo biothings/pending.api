9e42c0decce82f5bd725120a6910ff90450386b9
ctrl-schaff <schaffjohnathan@gmail.com>
2025-11-02 19:55:40 -0600

    Separate all uploading logic to the worker module

--- plugins/nodenorm/uploader.py
+++ plugins/nodenorm/uploader.py
@@ -1,34 +1,11 @@
-import concurrent.futures
-import hashlib
-import itertools
-import json
-import os
-import sqlite3
-from pathlib import Path
-from typing import Union
-
+import functools
 
 from biothings import config
 from biothings.hub.dataload.uploader import BaseSourceUploader
 from biothings.utils.manager import JobManager
 
-from .static import (
-    BASE_URL,
-    CONFLATION_LOOKUP_DATABASE,
-    DRUG_CHEMICAL_IDENTIFIER_FILES,
-    GENE_PROTEIN_IDENTIFER_FILES,
-    NODENORM_BIG_FILE_COLLECTION,
-    NODENORM_FILE_COLLECTION,
-    NODENORM_UPLOAD_CHUNKS,
-)
-from .worker import (
-    cleanup_curie_duplication,
-    create_identifiers_index,
-    create_identifiers_table,
-    create_mongo_identifiers_index,
-    subset_upload_worker,
-    update_identifier_collection,
-)
+from .static import BASE_URL
+from .worker import upload_process
 
 
 logger = config.logger
@@ -38,49 +15,28 @@ class NodeNormUploader(BaseSourceUploader):
     name = "nodenorm"
     __metadata__ = {"src_meta": {"url": BASE_URL}}
 
-    def _generate_data_filepaths(self) -> list[Union[str, Path]]:
-        """
-        Generates the files for processing in parallel
-        """
-        file_names = [*NODENORM_FILE_COLLECTION, *NODENORM_BIG_FILE_COLLECTION.keys()]
-        files = [Path(self.data_folder).joinpath(file) for file in file_names]
-        return files
-
     async def update_data(self, batch_size: int, job_manager: JobManager = None):
         """
         Primary mover for uploading the data to our backend
         """
+        pinfo = self.get_pinfo()
+        pinfo["step"] = "update_data"
+        got_error = False
         self.unprepare()
 
-        create_identifiers_table(self.data_folder)
+        job = await job_manager.defer_to_process(
+            pinfo, functools.partial(upload_process, self.data_folder, self.temp_collection_name)
+        )
 
-        with concurrent.futures.ProcessPoolExecutor(max_workers=1 * os.cpu_count()) as executor:
-            process_futures = []
-            for index, task in enumerate(self._build_offset_tasks()):
-                future = executor.submit(subset_upload_worker, **task)
-                process_futures.append(future)
+        def uploaded(f):
+            nonlocal got_error
+            if not isinstance(f.result(), int):
+                got_error = Exception(f"upload error (should have a int as returned value got {repr(f.result())}")
 
-            total_document_count = 0
-            for index, future in enumerate(concurrent.futures.as_completed(process_futures)):
-                try:
-                    identifiers = future.result()
-                except Exception as gen_exc:
-                    logger.exception(gen_exc)
-                    raise gen_exc
-                else:
-                    update_identifier_collection(self.data_folder, identifiers)
-                    total_document_count += len(identifiers)
-                    logger.debug(
-                        "Task %s completed | Update %s identifiers | Total identifiers %s",
-                        index,
-                        len(identifiers),
-                        total_document_count,
-                    )
-                    del identifiers
-
-        create_mongo_identifiers_index(self.temp_collection_name)
-        create_identifiers_index(self.data_folder)
-        cleanup_curie_duplication(self.data_folder, self.temp_collection_name)
+        job.add_done_callback(uploaded)
+        await job
+        if got_error:
+            raise got_error
 
         self.switch_collection()
         self.clean_archived_collections()
@@ -125,133 +81,3 @@ class NodeNormUploader(BaseSourceUploader):
             "all": {"type": "text"},
         }
         return mapping
-
-    def _build_offset_tasks(self):
-        """
-        Reads every file and builds an index file compiling the offset ranges
-        we want to read as a subset of the file processing.
-
-        These are all generated in a queue that we continually feed to multiprocessing queue
-        that is continually uploading to the backend database
-        """
-
-        def _populate_upload_arguments(input_file: Union[Path, str], num_partitions: int) -> list:
-            logger.info("Analyzing offsets for %s | number of partitions %s", input_file, num_partitions)
-            conflation_database = None
-            if input_file.name in DRUG_CHEMICAL_IDENTIFIER_FILES or input_file.name in GENE_PROTEIN_IDENTIFER_FILES:
-                data_folder = Path(input_file).absolute().resolve().parent
-                conflation_database = data_folder.joinpath(CONFLATION_LOOKUP_DATABASE)
-
-            offsets = NodeNormUploader.generate_file_offsets(input_file, num_partitions)
-
-            argument_collection = []
-            for offset_range in NodeNormUploader.window(offsets, 2):
-                offset_start = offset_range[0]
-                offset_end = offset_range[1]
-
-                arguments = {
-                    "input_file": input_file,
-                    "buffer_size": 5000,
-                    "offset_start": offset_start,
-                    "offset_end": offset_end,
-                    "collection_name": self.temp_collection_name,
-                    "conflation_database": conflation_database,
-                }
-                argument_collection.append(arguments)
-            return argument_collection
-
-        thread_futures = []
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            for filename, num_partitions in NODENORM_UPLOAD_CHUNKS.items():
-                filepath = Path(self.data_folder).joinpath(filename).resolve().absolute()
-                arguments = {"input_file": filepath, "num_partitions": num_partitions}
-                _populate_upload_arguments(**arguments)
-                future = executor.submit(_populate_upload_arguments, **arguments)
-                thread_futures.append(future)
-
-        concurrent.futures.wait(thread_futures, timeout=None, return_when=concurrent.futures.ALL_COMPLETED)
-
-        yield from itertools.chain.from_iterable([future.result() for future in thread_futures])
-
-    @classmethod
-    def generate_file_offsets(cls, file: Union[str, Path], num_partitions: int = None):
-        """
-        Generates an in-memory index for a file. Rather than storing the offset
-
-        We're primarily interested in parallel processsing a file in batches for uploading
-        to a database, so we don't need to store every offset as that would be a waste of memory.
-        Rather, based off the number of partitions we store the file size as we iterate over the file
-        so we can track the approximate percentage we've completed so we can store inteval markers while
-        chunking the file
-        """
-        if num_partitions is None:
-            num_partitions = 10
-
-        file = Path(file).absolute().resolve()
-        file_size_bytes = file.stat().st_size
-        file_index = file.with_suffix(".index")
-
-        logger.debug("Calculating SHA256 hashsum for file: %s", file)
-        file_hash = cls.sha256sum(file)
-
-        if file_index.exists():
-            with open(file_index, "r", encoding="utf-8") as index_handle:
-                previous_index = json.load(index_handle)
-
-            if previous_index["hash"] == file_hash:
-                return previous_index["index"]
-
-            logger.debug(
-                "Different hash found for file %s [%s, %s] [previous, current]. "
-                "Updating file offsets with the new file",
-                file,
-                previous_index["hash"],
-                file_hash,
-            )
-
-        partitions = [0]
-        marker = file_size_bytes / num_partitions
-        progress_bytes = 0
-        with open(file, "rb") as handle:
-            while line := handle.readline():
-                if progress_bytes >= marker:
-                    partitions.append(handle.tell())
-                    progress_bytes = 0
-                else:
-                    progress_bytes += len(line)
-            partitions.append(handle.tell())
-        with open(file_index, "w", encoding="utf-8") as index_handle:
-            index_handle.write(json.dumps({"index": partitions, "hash": file_hash}))
-        return partitions
-
-    @classmethod
-    def sha256sum(cls, file: Union[str, Path], buffer_size: int = None):
-        """
-        Generates a sha256 hashsum for a file to determine if the previously generated
-        index requires updating
-        """
-        if buffer_size is None:
-            buffer_size = 1024 * 128
-
-        filehash = hashlib.sha256()
-        buffer = bytearray(buffer_size)
-        file_view = memoryview(buffer)
-
-        with open(file, "rb", buffering=0) as file_handle:
-            while n := file_handle.readinto(file_view):
-                filehash.update(file_view[:n])
-        return filehash.hexdigest()
-
-    @classmethod
-    def window(cls, seq, n=2):
-        """
-        Returns a sliding window (of width n) over data from the iterable.
-        s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...
-        """
-        it = iter(seq)
-        result = tuple(itertools.islice(it, n))
-        if len(result) == n:
-            yield result
-        for elem in it:
-            result = result[1:] + (elem,)
-            yield result
--- plugins/nodenorm/worker.py
+++ plugins/nodenorm/worker.py
@@ -1,6 +1,8 @@
 import concurrent.futures
 import copy
+import hashlib
 import itertools
+import json
 import os
 import sqlite3
 import time
@@ -17,10 +19,178 @@ from biothings.utils.serializer import json_loads
 from biothings.utils.hub_db import get_src_db
 from biothings.utils.common import iter_n
 
-from .static import IDENTIFIER_LOOKUP_DATABASE
+from .static import (
+    CONFLATION_LOOKUP_DATABASE,
+    DRUG_CHEMICAL_IDENTIFIER_FILES,
+    GENE_PROTEIN_IDENTIFER_FILES,
+    IDENTIFIER_LOOKUP_DATABASE,
+    NODENORM_UPLOAD_CHUNKS,
+)
 
 
 logger = config.logger
+
+
+def upload_process(data_folder: Union[str, Path], collection_name: str):
+    create_identifiers_table(data_folder)
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=1 * os.cpu_count()) as executor:
+        process_futures = []
+        for index, task in enumerate(_build_offset_tasks(data_folder, collection_name)):
+            future = executor.submit(subset_upload_worker, **task)
+            process_futures.append(future)
+
+        total_document_count = 0
+        for index, future in enumerate(concurrent.futures.as_completed(process_futures)):
+            try:
+                identifiers = future.result()
+            except Exception as gen_exc:
+                logger.exception(gen_exc)
+                raise gen_exc
+            else:
+                update_identifier_collection(data_folder, identifiers)
+                total_document_count += len(identifiers)
+                logger.debug(
+                    "Task %s completed | Update %s identifiers | Total identifiers %s",
+                    index,
+                    len(identifiers),
+                    total_document_count,
+                )
+                del identifiers
+
+    create_mongo_identifiers_index(collection_name)
+    create_identifiers_index(data_folder)
+    cleanup_curie_duplication(data_folder, collection_name)
+
+
+def _build_offset_tasks(data_folder: Union[str, Path], collection_name: str):
+    """
+    Reads every file and builds an index file compiling the offset ranges
+    we want to read as a subset of the file processing.
+
+    These are all generated in a queue that we continually feed to multiprocessing queue
+    that is continually uploading to the backend database
+    """
+
+    def _populate_upload_arguments(input_file: Union[Path, str], num_partitions: int) -> list:
+        logger.info("Analyzing offsets for %s | number of partitions %s", input_file, num_partitions)
+        conflation_database = None
+        if input_file.name in DRUG_CHEMICAL_IDENTIFIER_FILES or input_file.name in GENE_PROTEIN_IDENTIFER_FILES:
+            data_folder = Path(input_file).absolute().resolve().parent
+            conflation_database = data_folder.joinpath(CONFLATION_LOOKUP_DATABASE)
+
+        offsets = generate_file_offsets(input_file, num_partitions)
+
+        argument_collection = []
+        for offset_range in window(offsets, 2):
+            offset_start = offset_range[0]
+            offset_end = offset_range[1]
+
+            arguments = {
+                "input_file": input_file,
+                "buffer_size": 5000,
+                "offset_start": offset_start,
+                "offset_end": offset_end,
+                "collection_name": collection_name,
+                "conflation_database": conflation_database,
+            }
+            argument_collection.append(arguments)
+        return argument_collection
+
+    thread_futures = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for filename, num_partitions in NODENORM_UPLOAD_CHUNKS.items():
+            filepath = Path(data_folder).joinpath(filename).resolve().absolute()
+            arguments = {"input_file": filepath, "num_partitions": num_partitions}
+            _populate_upload_arguments(**arguments)
+            future = executor.submit(_populate_upload_arguments, **arguments)
+            thread_futures.append(future)
+
+    concurrent.futures.wait(thread_futures, timeout=None, return_when=concurrent.futures.ALL_COMPLETED)
+
+    yield from itertools.chain.from_iterable([future.result() for future in thread_futures])
+
+
+def generate_file_offsets(file: Union[str, Path], num_partitions: int = None):
+    """
+    Generates an in-memory index for a file. Rather than storing the offset
+
+    We're primarily interested in parallel processsing a file in batches for uploading
+    to a database, so we don't need to store every offset as that would be a waste of memory.
+    Rather, based off the number of partitions we store the file size as we iterate over the file
+    so we can track the approximate percentage we've completed so we can store inteval markers while
+    chunking the file
+    """
+    if num_partitions is None:
+        num_partitions = 10
+
+    file = Path(file).absolute().resolve()
+    file_size_bytes = file.stat().st_size
+    file_index = file.with_suffix(".index")
+
+    logger.debug("Calculating SHA256 hashsum for file: %s", file)
+    file_hash = sha256sum(file)
+
+    if file_index.exists():
+        with open(file_index, "r", encoding="utf-8") as index_handle:
+            previous_index = json.load(index_handle)
+
+        if previous_index["hash"] == file_hash:
+            return previous_index["index"]
+
+        logger.debug(
+            "Different hash found for file %s [%s, %s] [previous, current]. " "Updating file offsets with the new file",
+            file,
+            previous_index["hash"],
+            file_hash,
+        )
+
+    partitions = [0]
+    marker = file_size_bytes / num_partitions
+    progress_bytes = 0
+    with open(file, "rb") as handle:
+        while line := handle.readline():
+            if progress_bytes >= marker:
+                partitions.append(handle.tell())
+                progress_bytes = 0
+            else:
+                progress_bytes += len(line)
+        partitions.append(handle.tell())
+    with open(file_index, "w", encoding="utf-8") as index_handle:
+        index_handle.write(json.dumps({"index": partitions, "hash": file_hash}))
+    return partitions
+
+
+def sha256sum(file: Union[str, Path], buffer_size: int = None):
+    """
+    Generates a sha256 hashsum for a file to determine if the previously generated
+    index requires updating
+    """
+    if buffer_size is None:
+        buffer_size = 1024 * 128
+
+    filehash = hashlib.sha256()
+    buffer = bytearray(buffer_size)
+    file_view = memoryview(buffer)
+
+    with open(file, "rb", buffering=0) as file_handle:
+        while n := file_handle.readinto(file_view):
+            filehash.update(file_view[:n])
+    return filehash.hexdigest()
+
+
+def window(seq: tuple, n: int = 2):
+    """
+    Returns a sliding window (of width n) over data from the iterable.
+    s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...
+    """
+    it = iter(seq)
+    result = tuple(itertools.islice(it, n))
+    if len(result) == n:
+        yield result
+    for elem in it:
+        result = result[1:] + (elem,)
+        yield result
 
 
 def subset_upload_worker(
