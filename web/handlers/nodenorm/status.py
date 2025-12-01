8012a7220da47b66db8fa52730e488737ba8c6f4
ctrl-schaff <schaffjohnathan@gmail.com>
2025-12-01 10:17:54 -0600

    Flush out remaining part for nodenorm status endpoint

--- web/handlers/nodenorm/status.py
+++ web/handlers/nodenorm/status.py
@@ -1,3 +1,7 @@
+import sys
+import types
+from urllib.parse import urlparse
+
 from biothings.web.handlers import BaseAPIHandler
 from biothings.web.services.namespace import BiothingsNamespace
 from tornado.web import HTTPError
@@ -7,26 +11,46 @@ class NodeNormStatusHandler(BaseAPIHandler):
     """
     Important Endpoints
     * /_cat/nodes
-
     """
 
     name = "status"
 
     async def get(self):
+        attributes = [
+            "name",
+            "cpu",
+            "disk.avail",
+            "disk.total",
+            "disk.used",
+            "disk.used_percent",
+            "heap.current",
+            "heap.max",
+            "load_1m",
+            "load_5m",
+            "load_15m",
+            "uptime,version",
+        ]
         try:
-            cat_nodes_response = await self.biothings.elasticsearch.async_client.cat.nodes()
-            nodes_info_response = await self.biothings.elasticsearch.async_client.nodes.info()
-            nodes_stats_response = await self.biothings.elasticsearch.async_client.nodes.stats()
+            h_string = ",".join(attributes)
+            cat_nodes_response = await self.biothings.elasticsearch.async_client.cat.nodes(format="json", h=h_string)
+            nodes_status = {node["name"]: node for node in cat_nodes_response}
+            nodes = {"elasticsearch": {"nodes": nodes_status}}
         except Exception as gen_exc:
             network_error = HTTPError(
                 detail="Unable to access the elasticsearch index for type information", status_code=500
             )
             raise network_error from gen_exc
-        breakpoint()
+        else:
+            compendia_url = self.biothings.metadata.biothing_metadata["node"]["src"]["nodenorm"]["url"]
+            parsed_compendia_url = urlparse(compendia_url)
+            babel_version = parsed_compendia_url.path.split("/")[-2]
+            babel_markdown = f"https://github.com/ncatstranslator/Babel/blob/master/releases/{babel_version}.md"
 
-        status_response = {
-            "babel_version": "2025sep1",
-            "babel_version_url": "https://github.com/ncatstranslator/Babel/blob/master/releases/2025sep1.md",
-        }
+            status_response = {
+                "babel_compendia": compendia_url,
+                "babel_version": babel_version,
+                "babel_version_url": babel_markdown,
+                **nodes,
+            }
 
         self.finish(status_response)
